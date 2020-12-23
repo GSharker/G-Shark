721e204117132fc5a08d109bc42717361e86ddd1
Cesare Caoduro <Cesare.Caoduro@aecom.com>
2020-12-23 16:08:57 +1100

    added signatures to Mat

--- VerbNurbsSharp/Core/Mat.cs
+++ VerbNurbsSharp/Core/Mat.cs
@@ -8,5 +8,27 @@ namespace VerbNurbsSharp.Core
 {
     public class Mat
     {
+        public static Matrix Mul(double a, Matrix b) => throw new NotImplementedException();
+        public static Matrix Mutl(Matrix a, Matrix b) => throw new NotImplementedException();
+        public static Matrix Add(Matrix a, Matrix b) => throw new NotImplementedException();
+        public static Matrix Add(Matrix a, double b) => throw new NotImplementedException();
+        public static Matrix Sub(Matrix a, Matrix b) => throw new NotImplementedException();
+        public static Vector Dot(Matrix a, Vector b) => throw new NotImplementedException();
+        public static Vector Identity(int i) => throw new NotImplementedException();
+        public static Matrix Transpose(Matrix a) => throw new NotImplementedException();
+        public static Vector Solve(Matrix a, Vector b) => throw new NotImplementedException();
+        public static Vector LUSolve(LUDecomp LUP, Vector b) => throw new NotImplementedException();
+        public static LUDecomp LU(Matrix a) => throw new NotImplementedException();
+    }
+
+    public class LUDecomp
+    {
+        public Matrix LU { get; set; }
+        public List<int> P { get; set; }
+        public LUDecomp(Matrix lU, List<int> p)
+        {
+            LU = lU;
+            P = p;
+        }
     }
 }
