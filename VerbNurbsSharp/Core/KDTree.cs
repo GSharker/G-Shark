59e6f8a78048eede54607a6860215b71dd4bf191
Cesare Caoduro <Cesare.Caoduro@aecom.com>
2020-12-23 21:47:57 +1100

    Initialized KDTree

---
+++ VerbNurbsSharp/Core/KDTree.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerbNurbsSharp.Core.KDTree
+{
+    public class KDTree<T>
+    {
+        public List<KDPoints<T>> Points { get; set; }
+        //private var distanceFunction : Point -> Point -> Float;
+
+        public int Dim { get; set; } = 3;
+        public KDNode<T> Root { get; set; }
+
+        public KDTree(List<KDPoints<T>> points)
+        {
+            Points = points;
+            Dim = points[0].Point.Count;
+            //this.distanceFunction = distanceFunction;
+            Root = BuildTree(points, 0, null);
+        }
+
+        private KDNode<T> BuildTree(List<KDPoints<T>> points, int depth, KDNode<T> parent) => throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// A node in a KdTree
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class KDNode<T>
+    {
+        /// <summary>
+        /// The point itself
+        /// </summary>
+        public KDPoints<T> KDPoint { get; set; }
+
+        /// <summary>
+        /// The left child
+        /// </summary>
+        public KDNode<T> Left { get; set; }
+
+        /// <summary>
+        /// The right child
+        /// </summary>
+        public KDNode<T> Right { get; set; }
+
+        /// <summary>
+        /// The parent of the node
+        /// </summary>
+        public KDNode<T> Parent { get; set; }
+
+        /// <summary>
+        /// The dimensionality of the point
+        /// </summary>
+        public int Dimension { get; set; }
+
+        public KDNode(KDPoints<T> kDPoint, int dimension, KDNode<T> parent)
+        {
+            KDPoint = kDPoint;
+            Dimension = dimension;
+            Parent = parent;
+            Left = null;
+            Right = null;
+        }
+
+    }
+
+    /// <summary>
+    /// A point in a KdTree
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class KDPoints<T>
+    {
+        /// <summary>
+        /// The point
+        /// </summary>
+        public Point Point { get; set; }
+
+        /// <summary>
+        /// An arbitrary object to attach
+        /// </summary>
+        public T Obj { get; set; }
+
+        public KDPoints(Point point, T obj)
+        {
+            Point = point;
+            Obj = obj;
+        }
+    }
+}
