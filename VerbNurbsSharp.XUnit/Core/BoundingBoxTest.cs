6d76e3b82cb23ec6746b30f9cb54d4c74daadf3c
Mirco Bianchini <sonomirco@gmail.com>
2021-01-02 11:52:51 +1100

    Updated the asserts for the bounding box tests using fluent assert.

--- VerbNurbsSharp.XUnit/Core/BoundingBoxTest.cs
+++ VerbNurbsSharp.XUnit/Core/BoundingBoxTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using FluentAssertions;
 using VerbNurbsSharp.Core;
 using VerbNurbsSharp.Geometry;
 using Xunit;
@@ -24,41 +25,49 @@ namespace VerbNurbsSharpTest
             BoundingBox bBox = new BoundingBox(pts);
 
             _testOutput.WriteLine(bBox.ToString());
-            Assert.NotNull(bBox);
-            Assert.Equal(min, bBox.Min);
-            Assert.Equal(max, bBox.Max);
+            bBox.Should().NotBeNull();
+            bBox.Min.Should().BeEquivalentTo(min);
+            bBox.Max.Should().BeEquivalentTo(max);
         }
 
+        [Trait("Category", "BoundingBox")]
         [Fact]
         public void ReturnTrue_IfAPoint_Is_Contained_Into_TheBoundingBox()
         {
             Point conteinedPt = new Point() {2.5, 4.5, 0.0};
 
             BoundingBox bBox = new BoundingBox(BoundingBoxCollection.BoundingBoxFrom5Points());
+            bool containsResult = bBox.Contains(conteinedPt, false);
 
-            Assert.True(bBox.Contains(conteinedPt, false));
+            containsResult.Should().BeTrue();
         }
 
+        [Trait("Category", "BoundingBox")]
         [Fact]
         public void ReturnFalse_IfAPoint_Is_Outside_TheBoundingBox()
         {
             Point externalPt = new Point() { 12.4, 5.0, 0.0 };
 
             BoundingBox bBox = new BoundingBox(BoundingBoxCollection.BoundingBoxFrom5Points());
+            bool containsResult = bBox.Contains(externalPt, false);
 
-            Assert.False(bBox.Contains(externalPt, false));
+            containsResult.Should().BeFalse();
         }
 
+        [Trait("Category", "BoundingBox")]
         [Theory]
         [MemberData(nameof(BoundingBoxCollection.BoundingBoxIntersections), MemberType = typeof(BoundingBoxCollection))]
         public void ReturnTrue_If_TwoBoundingBoxes_Intersect(List<Point> ptsBBox1, List<Point> ptsBBox2, bool result)
         {
             BoundingBox bBox1 = new BoundingBox(ptsBBox1);
             BoundingBox bBox2 = new BoundingBox(ptsBBox2);
+
             bool intersectionResult = BoundingBox.AreOverlapping(bBox1, bBox2, 0.0);
-            Assert.Equal(result, intersectionResult);
+
+            intersectionResult.Should().Be(result);
         }
 
+        [Trait("Category", "BoundingBox")]
         [Fact]
         public void IntersectReturns_UnsetBBox_If_OneOfTheTwoBBoxes_IsNotInitialized()
         {
@@ -67,11 +76,12 @@ namespace VerbNurbsSharpTest
 
             BoundingBox bBoxResult = BoundingBox.Intersect(bBox1, bBox2);
 
-            Assert.False(bBoxResult.IsValid);
-            Assert.Equal(Point.Unset,bBoxResult.Min);
-            Assert.Equal(Point.Unset, bBoxResult.Max);
+            bBoxResult.IsValid.Should().BeFalse();
+            bBoxResult.Max.Should().BeEquivalentTo(Point.Unset);
+            bBoxResult.Min.Should().BeEquivalentTo(Point.Unset);
         }
 
+        [Trait("Category", "BoundingBox")]
         [Fact]
         public void IntersectReturns_UnsetBBox_If_ThereIsNotIntersection()
         {
@@ -80,11 +90,12 @@ namespace VerbNurbsSharpTest
 
             BoundingBox bBoxResult = BoundingBox.Intersect(bBox1, bBox2);
 
-            Assert.False(bBoxResult.IsValid);
-            Assert.Equal(Point.Unset, bBoxResult.Min);
-            Assert.Equal(Point.Unset, bBoxResult.Max);
+            bBoxResult.IsValid.Should().BeFalse();
+            bBoxResult.Max.Should().BeEquivalentTo(Point.Unset);
+            bBoxResult.Min.Should().BeEquivalentTo(Point.Unset);
         }
 
+        [Trait("Category", "BoundingBox")]
         [Fact]
         public void IntersectReturns_BBox_As_Intersection_Of_Two_BBoxes()
         {
@@ -94,58 +105,67 @@ namespace VerbNurbsSharpTest
 
             BoundingBox bBox1 = new BoundingBox(BoundingBoxCollection.BoundingBoxFrom5Points());
             BoundingBox bBox2 = new BoundingBox(pts2);
-
             BoundingBox bBoxResult = bBox1.Intersect(bBox2);
-            Assert.True(bBoxResult.IsValid);
-            Assert.Equal(bBoxResult.Min, bBox2.Min);
-            Assert.Equal(bBoxResult.Max, bBox1.Max);
+
+            bBoxResult.IsValid.Should().BeTrue();
+            bBoxResult.Max.Should().BeEquivalentTo(bBox1.Max);
+            bBoxResult.Min.Should().BeEquivalentTo(bBox2.Min);
         }
 
+        [Trait("Category", "BoundingBox")]
         [Fact]
         public void Return_A_BBox_NotInitialized()
         {
             BoundingBox bBox = new BoundingBox(BoundingBoxCollection.BoundingBoxFrom5Points());
+
             bBox.Clear();
 
-            Assert.False(bBox.IsValid);
+            bBox.IsValid.Should().BeFalse();
         }
 
+        [Trait("Category", "BoundingBox")]
         [Theory]
         [MemberData(nameof(BoundingBoxCollection.BoundingBoxAxisLength), MemberType = typeof(BoundingBoxCollection))]
         public void Return_ACollection_Of_GetAxisLength(List<Point> pts, int index, double length)
         {
             BoundingBox bBox = new BoundingBox(pts);
+
             double lengthResult = bBox.GetAxisLength(index);
-            Assert.Equal(length, lengthResult);
+
+            lengthResult.Should().Be(length);
         }
 
+        [Trait("Category", "BoundingBox")]
         [Fact]
         public void Return_TheLongestAxis()
         {
             BoundingBox bBox = new BoundingBox(BoundingBoxCollection.BoundingBoxWithZValue());
+
             int longestAxis = bBox.GetLongestAxis();
-            Assert.Equal(1,longestAxis);
+
+            longestAxis.Should().Be(1);
         }
 
+        [Trait("Category", "BoundingBox")]
         [Fact]
         public void Return_ABooleanUnion_BetweenTwo_BoundingBoxes()
         {
             Point pt1 = new Point() { 5d, 5d, 0d };
             Point pt2 = new Point() { -15d, -13d, -5d };
-            List<Point> pts2 = new List<Point>() { pt1, pt2 };
+            List<Point> pts = new List<Point>() { pt1, pt2 };
             Point pMax = new Point() {10, 10, 0};
 
             BoundingBox bBox1 = new BoundingBox(BoundingBoxCollection.BoundingBoxFrom5Points());
-            BoundingBox bBox2 = new BoundingBox(pts2);
-
+            BoundingBox bBox2 = new BoundingBox(pts);
             BoundingBox bBoxResult = bBox1.Union(bBox2);
 
             _testOutput.WriteLine(bBoxResult.ToString());
-            Assert.True(bBoxResult.IsValid);
-            Assert.Equal(pt2, bBoxResult.Min);
-            Assert.Equal(pMax, bBoxResult.Max);
+            bBoxResult.IsValid.Should().BeTrue();
+            bBoxResult.Max.Should().BeEquivalentTo(pMax);
+            bBoxResult.Min.Should().BeEquivalentTo(pt2);
         }
 
+        [Trait("Category", "BoundingBox")]
         [Fact]
         public void Union_Returns_TheValidBoundingBox_IfOther_IsNotValid()
         {
@@ -154,7 +174,7 @@ namespace VerbNurbsSharpTest
 
             BoundingBox bBoxResult = BoundingBox.Union(bBox1, bBox2);
 
-            Assert.Equal(bBoxResult, bBox2);
+            bBoxResult.Should().Be(bBox2);
         }
     }
 }
