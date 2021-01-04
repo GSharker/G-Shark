3e6d5fd685a80fe2b38d41fc656a743646d35da2
Cesare Caoduro <Cesare.Caoduro@aecom.com>
2021-01-04 13:56:34 +1100

    Initialized BinaryHeap signature

---
+++ VerbNurbsSharp/Core/BinaryHeap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerbNurbsSharp.Core.BinaryHeap
+{
+    public interface IBinaryHeap<T>
+    {
+    }
+
+    public class BinaryHeap<T> : IBinaryHeap<T>
+    {
+    }
+}
