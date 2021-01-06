b3434b1cceb750c73c1062da8407595bfe0ef7f6
peggylin-hub <peggy.lin.syd@gmail.com>
2021-01-06 11:37:36 +1100

    Update Divide, Eval, Make and Modify class

--- VerbNurbsSharp/Evaluation/Make.cs
+++ VerbNurbsSharp/Evaluation/Make.cs
@@ -15,7 +15,7 @@ namespace VerbNurbsSharp.Evaluation
 
         }
 
-        public static NurbsCurveData RationalBezierCurve(List<Point> controlPoints, List<double> weights = null)
+        public static NurbsCurve RationalBezierCurve(List<Point> controlPoints, List<double> weights = null)
         {
             var degree = controlPoints.Count - 1;
             var knots = new KnotArray();
@@ -25,10 +25,176 @@ namespace VerbNurbsSharp.Evaluation
                 knots.Add(1.0);
             if (weights == null)
                 weights = Sets.RepeatData(1.0, controlPoints.Count);
-            return new NurbsCurveData(degree, knots, Eval.Homogenize1d(controlPoints, weights));
+            return new NurbsCurve(degree, knots, Eval.Homogenize1d(controlPoints, weights));
                 weights = Constants.Rep(controlPoints.Count, 1.0);
             //return new NurbsCurveData(degree, knots, Eval.Homogenize1d(controlPoints, weights));
             return null;
+        }
+
+        public static NurbsCurve ClonedCurve(NurbsCurve curve)
+        {
+            return new NurbsCurve(curve.Degree, curve.Knots, curve.ControlPoints);
+        }
+
+
+        //////////////////////////// =================================== not implemented yet ================================== ///////////////////
+
+        /// <summary>
+        /// Generate a surface by translating a profile curve along a rail curve
+        /// </summary>
+        /// <param name="profile">NurbCurveData Profile</param>
+        /// <param name="rail">NurbCurveData Rail</param>
+        /// <returns></returns>
+        public static NurbsSurface RationalTranslationalSurface(NurbsCurve profile, NurbsCurve rail)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// Extract the boundary curves from a surface
+        /// </summary>
+        /// <param name="surface"></param>
+        /// <returns>an List containing 4 elements, first 2 curves in the V direction, then 2 curves in the U direction</returns>
+        public static List<NurbsCurve> SurfaceBoundaryCurves(NurbsSurface surface)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static NurbsCurve SurfaceIsocurve(NurbsSurface surface, double u, bool useV = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static NurbsSurface LoftedSurface(List<NurbsCurve> curves, int degreeV = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="p1">first point in counter-clockwise form</param>
+        /// <param name="p2">second point in counter-clockwise form</param>
+        /// <param name="p3">third point in counter-clockwise form</param>
+        /// <param name="p4">forth point in counter-clockwise form</param>
+        /// <param name="degree"></param>
+        /// <returns></returns>
+        public static NurbsSurface FourPointSurface(Point p1, Point p2, Point p3, Point p4, int degree = 3)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generate the control points, weights, and knots of an elliptical arc
+        /// </summary>
+        /// <param name="center">the center</param>
+        /// <param name="xaxis">the scaled x axis</param>
+        /// <param name="yaxis">the scaled y axis</param>
+        /// <param name="startAngle">start angle of the ellipse arc, between 0 and 2pi, where 0 points at the xaxis</param>
+        /// <param name="endAngle">end angle of the arc, between 0 and 2pi, greater than the start angle</param>
+        /// <returns>a NurbsCurveData object representing a NURBS curve</returns>
+        public static NurbsCurve EllipseArc(Point center, Vector xaxis, Point yaxis, double startAngle, double endAngle)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generate the control points, weights, and knots of an arbitrary arc
+        /// (Corresponds to Algorithm A7.1 from Piegl & Tiller)
+        /// </summary>
+        /// <param name="center">the center of the arc</param>
+        /// <param name="xaxis">the xaxis of the arc</param>
+        /// <param name="yaxis">orthogonal yaxis of the arc</param>
+        /// <param name="radius">radius of the arc</param>
+        /// <param name="startAngle">start angle of the arc, between 0 and 2pi</param>
+        /// <param name="endAngle">end angle of the arc, between 0 and 2pi, greater than the start angle</param>
+        /// <returns>a NurbsCurveData object representing a NURBS curve</returns>
+        public static NurbsCurve Arc(Point center, Vector xaxis, Vector yaxis, double radius, double startAngle, double endAngle)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generate the control points, weights, and knots of a polyline curve
+        /// </summary>
+        /// <param name="pts">array of points in curve</param>
+        /// <returns>a NurbsCurveData object representing a NURBS curve</returns>
+        public static NurbsCurve Polyline(List<Point> pts)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generate the control points, weights, and knots of an extruded surface
+        /// </summary>
+        /// <param name="axis">axis of the extrusion</param>
+        /// <param name="length">length of the extrusion</param>
+        /// <param name="profile">a NurbsCurveData object representing a NURBS surface</param>
+        /// <returns>an object with the following properties: controlPoints, weights, knots, degree</returns>
+        public static NurbsSurface ExtrudedSurface(Vector axis, double length, NurbsCurve profile)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generate the control points, weights, and knots of a cylinder
+        /// </summary>
+        /// <param name="axis">normalized axis of cylinder</param>
+        /// <param name="xaxis">xaxis in plane of cylinder</param>
+        /// <param name="basePt">position of base of cylinder</param>
+        /// <param name="height">height from base to top</param>
+        /// <param name="radius">radius of the cylinder</param>
+        /// <returns>an object with the following properties: controlPoints, weights, knotsU, knotsV, degreeU, degreeV</returns>
+        public static NurbsSurface CylindricalSurface(Vector axis, Vector xaxis, Point basePt, double height, double radius)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="profile"></param>
+        /// <param name="center"></param>
+        /// <param name="axis"></param>
+        /// <param name="theta"></param>
+        /// <returns></returns>
+        public static NurbsSurface RevolvedSurface(NurbsCurve profile, Point center, Vector axis, double theta)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generate the control points, weights, and knots of a sphere
+        /// </summary>
+        /// <param name="center">the center of the sphere</param>
+        /// <param name="axis">normalized axis of sphere</param>
+        /// <param name="xaxis">vector perpendicular to axis of sphere, starting the rotation of the sphere</param>
+        /// <param name="radius">radius of the sphere</param>
+        /// <returns>an object with the following properties: controlPoints, weights, knotsU, knotsV, degreeU, degreeV</returns>
+        public static NurbsSurface SphericalSurface(Point center, Vector axis, Vector xaxis, double radius)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generate the control points, weights, and knots of a cone
+        /// </summary>
+        /// <param name="axis">normalized axis of cone</param>
+        /// <param name="xaxis"></param>
+        /// <param name="basePt">position of base of cone</param>
+        /// <param name="height">height from base to tip</param>
+        /// <param name="radius">radius at the base of the cone</param>
+        /// <returns></returns>
+        public static NurbsSurface ConicalSurface(Vector axis, Vector xaxis, Point basePt, double height, double radius)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static NurbsCurve RationalInterpCurve(List<List<double>> points, int degree, bool homogeneousPoints = false, Point start_tangent = null, Point end_tangent = null)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- VerbNurbsSharp/Evaluation/Modify.cs
+++ VerbNurbsSharp/Evaluation/Modify.cs
@@ -17,16 +17,191 @@ namespace VerbNurbsSharp.Evaluation
 		/// <param name="curve"></param>
 		/// <param name="knotsToInsert"></param>
 		/// <returns></returns>
-		public static NurbsCurveData curveKnotRefine(NurbsCurveData curve, List<double> knotsToInsert)
+		public static NurbsCurve CurveKnotRefine(NurbsCurve curve, List<double> knotsToInsert)
 		{
-			//if (knotsToInsert.Count == 0)
-			//	return Make.clonedCurve(curve);
+			if (knotsToInsert.Count == 0)
+				return Make.ClonedCurve(curve);
 
 			int degree = curve.Degree;
 			List<Point> controlPoints = curve.ControlPoints;
 			KnotArray knots = curve.Knots;
 
-			return null;
+			int n = controlPoints.Count - 1;
+			int m = n + degree + 1;
+			int r = knotsToInsert.Count - 1;
+			int a = Eval.KnotSpan(degree, knotsToInsert[0], knots);
+			int b = Eval.KnotSpan(degree, knotsToInsert[r], knots);
+			List<Point> controlPoints_post = new List<Point>();
+			KnotArray knots_post = new KnotArray();
+
+			//new control points
+			for (int i = 0; i <= a - degree; i++)
+				controlPoints_post[i] = controlPoints[i];
+
+			for (int i = b - 1; i <= n; i++)
+				controlPoints_post[i + r + 1] = controlPoints[i];
+
+			//new knot vector
+			for (int i = 0; i <= a; i++)
+				knots_post[i] = knots[i];
+
+			for (int i = b + degree; i <= m; i++)
+				knots_post[i + r + 1] = knots[i];
+
+			int ii = b + degree + 1;
+			int k = b + degree + r;
+			int j = r;
+			while (j >= 0)
+			{
+				while (knotsToInsert[j] <= knots[ii] && ii > a)
+				{
+					controlPoints_post[k - degree - 1] = controlPoints[ii - degree - 1];
+					knots_post[k] = knots[ii];
+					k = k - 1;
+					ii = ii - 1;
+				}
+
+				controlPoints_post[k - degree - 1] = controlPoints_post[k - degree];
+
+				for (int i = 1; i <= degree; i++)
+				{
+					int ind = k - degree + 1;
+					double alfa = knots_post[k + 1] - knotsToInsert[j];
+
+					if (Math.Abs(alfa) < Constants.EPSILON)
+						controlPoints_post[ind - 1] = controlPoints_post[ind];
+					else
+					{
+						alfa = alfa / (knots_post[k + 1] - knots[ii - degree + 1]);
+						controlPoints_post[ind - 1] = Vector.Add(Vector.Mul(alfa, controlPoints_post[ind - 1]), Vector.Mul((1.0 - alfa), controlPoints_post[ind]));
+					}
+				}
+
+				knots_post[k] = knotsToInsert[j];
+				k = k - 1;
+				j--;
+			}
+
+
+			return new NurbsCurve(degree, knots_post, controlPoints_post);
+		}
+
+		private static int Imin(int a, int b)
+		{
+			return a < b ? a : b;
+		}
+
+		private static int Imax(int a, int b)
+		{
+			return a > b ? a : b;
+		}
+
+
+		//////////////////////////// =================================== not implemented yet ================================== ///////////////////
+
+		/// <summary>
+		/// Reverses the parameterization of a NURBS curve. The domain is unaffected.
+		/// </summary>
+		/// <param name="curve">A NURBS curve to be reversed</param>
+		/// <returns>A new NURBS curve with a reversed parameterization</returns>
+		public static NurbsCurve CurveReverse(NurbsCurve curve)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static NurbsSurface SurfaceReverse(NurbsSurface surface, bool useV = false)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Reverse a knot vector
+		/// </summary>
+		/// <param name="knots">An array of knots</param>
+		/// <returns>The reversed array of knots</returns>
+		public static KnotArray KnotsReverse(KnotArray knots)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Unify the knot vectors of a collection of NURBS curves. This can be used, for example, is used for lofting between curves.
+		/// </summary>
+		/// <param name="curves">An array of NURBS curves</param>
+		/// <returns>A collection of NURBS curves, all with the same knot vector</returns>
+		public static List<NurbsCurve> UnifyCurveKnotVectors(List<NurbsCurve> curves)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Elevate the degree of a NURBS curve
+		/// </summary>
+		/// <param name="curve">The curve to elevate</param>
+		/// <param name="finalDegree">The expected final degree</param>
+		/// <returns>The NURBS curve after degree elevation - if the supplied degree is <= the curve is returned unmodified</returns>
+		public static NurbsCurve CurveElevateDegree(NurbsCurve curve, int finalDegree)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Transform a NURBS surface using a matrix
+		/// </summary>
+		/// <param name="surfaceData">The surface to transform</param>
+		/// <param name="mat">The matrix to use for the transform - the dimensions should be the dimension of the surface + 1 in both directions</param>
+		/// <returns>A new NURBS surface after transformation</returns>
+		public static NurbsSurface RationalSurfaceTransform(NurbsSurface surfaceData, Matrix mat)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Transform a NURBS curve using a matrix
+		/// </summary>
+		/// <param name="curve">The curve to transform</param>
+		/// <param name="mat">The matrix to use for the transform - the dimensions should be the dimension of the curve + 1 in both directions</param>
+		/// <returns>A new NURBS surface after transformation</returns>
+		public static NurbsCurve RationalCurveTransform(NurbsCurve curve, Matrix mat)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Perform knot refinement on a NURBS surface by inserting knots at various parameters
+		/// </summary>
+		/// <param name="surface">The surface to insert the knots into</param>
+		/// <param name="knotsToInsert">The knots to insert - an array of parameter positions within the surface domain</param>
+		/// <param name="useV">Whether to insert in the U direction or V direction of the surface</param>
+		/// <returns>A new NURBS surface with the knots inserted</returns>
+		public static NurbsSurface SurfaceKnotRefine(NurbsSurface surface, List<double> knotsToInsert, bool useV)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Decompose a NURBS curve into a collection of bezier's.  Useful
+		/// as each bezier fits into it's convex hull.  This is a useful starting
+		/// point for intersection, closest point, divide & conquer algorithms
+		/// </summary>
+		/// <param name="curve">NurbsCurveData object representing the curve</param>
+		/// <returns>List of NurbsCurveData objects, defined by degree, knots, and control points</returns>
+		public static List<NurbsCurve> DecomposeCurveIntoBeziers(NurbsCurve curve)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Insert a knot along a rational curve.  Note that this algorithm only works
+		/// for r + s <= degree, where s is the initial multiplicity (number of duplicates) of the knot.
+		/// </summary>
+		/// <param name="curve"></param>
+		/// <param name="u"></param>
+		/// <param name="r"></param>
+		/// <returns></returns>
+		public static NurbsCurve CurveKnotInsert(NurbsCurve curve, double u, int r)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
