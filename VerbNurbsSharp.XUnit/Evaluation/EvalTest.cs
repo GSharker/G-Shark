1deb0c8b524656aa52065d493ae48e87ab9d2d92
Mirco Bianchini <sonomirco@gmail.com>
2021-01-07 12:26:41 +1100

    Implemented test for Eval.Homogenized1d.

---
+++ VerbNurbsSharp.XUnit/Evaluation/EvalTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using VerbNurbsSharp.Core;
+using VerbNurbsSharp.Evaluation;
+using Xunit;
+
+namespace VerbNurbsSharp.XUnit.Evaluation
+{
+    [Trait("Category", "Eval")]
+    public class EvalTest
+    {
+        public static IEnumerable<object[]> Homogenized1dData => new List<object[]>
+        {
+            new object[]
+            {
+                new List<double>() {0.5, 0.5, 0.5},
+                new List<Vector>()
+                {
+                    new Vector() {0.0, 0.0, 0.0, 0.5},
+                    new Vector() {1.25, -1.25, 0.0, 0.5},
+                    new Vector() {2.5, 0.0, 0.0, 0.5}
+                }
+            },
+
+            new object[]
+            {
+                new List<double>() {0.5},
+                new List<Vector>()
+                {
+                    new Vector() {0.0, 0.0, 0.0, 0.5},
+                    new Vector() {2.5, -2.5, 0.0, 1.0},
+                    new Vector() {5.0, 0.0, 0.0, 1.0}
+                }
+            },
+
+            new object[]
+            {
+                null,
+                new List<Vector>()
+                {
+                    new Vector() {0.0, 0.0, 0.0, 1.0},
+                    new Vector() {2.5, -2.5, 0.0, 1.0},
+                    new Vector() {5.0, 0.0, 0.0, 1.0}
+                }
+            }
+        };
+
+        [Fact]
+        public void EvalHomogenized1d_ThrowsAnException_IfTheSetOfWeights_IsBiggerThanControlPts()
+        {
+            var controlPts = new List<Vector>();
+            var weights = new List<double>(){1.0,1.5,1.0};
+
+            Func<object> resultFunction = () => Eval.Homogenize1d(controlPts, weights);
+
+            resultFunction.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Theory]
+        [MemberData(nameof(Homogenized1dData))]
+        public void NewSetOfControlPoints_Homogenized1d(List<double> weights, List<Vector> controlPtsExpected)
+        {
+            var controlPts = new List<Vector>()
+            {
+                new Vector() {0.0, 0.0, 0},
+                new Vector() {2.5, -2.5, 0},
+                new Vector() {5.0, 0.0, 0}
+            };
+
+            var newControlPts = Eval.Homogenize1d(controlPts, weights);
+
+            newControlPts.Should().BeEquivalentTo(controlPtsExpected);
+        }
+    }
+}
--- VerbNurbsSharp/Evaluation/Eval.cs
+++ VerbNurbsSharp/Evaluation/Eval.cs
@@ -1,6 +1,8 @@
 ﻿using VerbNurbsSharp.Core;
 using System.Collections.Generic;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
 
 namespace VerbNurbsSharp.Evaluation
 {
@@ -51,10 +53,45 @@ namespace VerbNurbsSharp.Evaluation
 
             return mid;
         }
-
-        public static List<Vector> Homogenize1d(List<Vector> controlPoints, List<double> weights)
+        /// <summary>
+        /// Transform a 1d array of points into their homogeneous equivalents.
+        /// </summary>
+        /// <param name="controlPoints"> Control points, a 2d set of size (m x dim).</param>
+        /// <param name="weights">Control point weights, the same size as the set of control points (m x 1).</param>
+        /// <returns> 1d set of control points where each point is (wi*pi, wi) where wi the ith control point weight and pi is the ith control point, hence the dimension of the point is dim + 1.</returns>
+        public static List<Vector> Homogenize1d(List<Vector> controlPoints, List<double> weights = null)
         {
-            throw new NotImplementedException();
+            var neWeights = weights;
+            if (weights == null || weights.Count == 0)
+                neWeights = Sets.RepeatData(1.0, controlPoints.Count);
+            else
+            {
+                if (controlPoints.Count < weights.Count) throw new ArgumentOutOfRangeException(nameof(weights), "The weights set is bigger than the control points, it must be the same dimension");
+                if (controlPoints.Count > weights.Count)
+                {
+                    var diff = controlPoints.Count - weights.Count;
+                    var dataFilled = Sets.RepeatData(1.0, diff);
+                    neWeights.AddRange(dataFilled);
+                }
+            }
+
+            var rows = controlPoints.Count;
+            var dim = controlPoints[0].Count;
+            var controlPtsHomogenized = new List<Vector>();
+
+            for (int i = 0; i < rows; i++)
+            {
+                var tempPt = new Vector();
+                for (int j = 0; j < dim; j++)
+                {
+                    tempPt.Add(controlPoints[i][j] * neWeights[i]);
+                }
+                // Added the weight to the point.
+                tempPt.Add(neWeights[i]);
+                controlPtsHomogenized.Add(tempPt);
+            }
+
+            return controlPtsHomogenized;
         }
     }
 }
