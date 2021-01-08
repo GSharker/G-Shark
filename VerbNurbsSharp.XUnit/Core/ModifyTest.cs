12960e47561393b5ab30cc9dde721d30c96694b7
linp3 <linp3@AUSYD1LT1924>
2021-01-08 18:16:36 +1100

    Added ModifyTestCollection and update ModifyTest

--- VerbNurbsSharp.XUnit/Core/ModifyTest.cs
+++ VerbNurbsSharp.XUnit/Core/ModifyTest.cs
@@ -19,23 +19,28 @@ namespace VerbNurbsSharp.XUnit.Core
 			_testOutput = testOutput;
 		}
 
-		//[Fact]
-		//public void Check_CurveKnotRefine(
-		//	int inputDegree, 
-		//	List<double> inputKnots, 
-		//	List<Vector> inputControlPts, 
-		//	List<double>knotsToInsert,
-		//	int outputDegree,
-		//	List<double> outputKnots,
-		//	List<Vector> outputControlPts)
-		//{
-		//	//arrange
-		//	NurbsCurve inputCurve = new NurbsCurve(inputDegree, (KnotArray)inputKnots, inputControlPts);
-			
-		//	//act
-		//	NurbsCurve expectedCurve = Evaluation.Modify.CurveKnotRefine(inputCurve, knotsToInsert);
-		//	//assert
-		//	Assert.Equal(expectedCurve.Degree, inputCurve.Degree);
-		//}
+		[Theory]
+		[MemberData(nameof(ModifyTestCollection.CurveKnotRefindData), MemberType = typeof(ModifyTestCollection))]
+		public void Check_CurveKnotRefine(
+			int inputDegree,
+			KnotArray inputKnots,
+			List<Vector> inputControlPts,
+			List<double> knotsToInsert,
+			int outputDegree,
+			KnotArray outputKnots,
+			List<Vector> outputControlPts)
+		{
+			//arrange
+			NurbsCurve inputCurve = new NurbsCurve(inputDegree, inputKnots, inputControlPts);
+
+			//act
+			NurbsCurve expectedCurve = Evaluation.Modify.CurveKnotRefine(inputCurve, knotsToInsert);
+			//assert
+			Assert.Equal(expectedCurve.Degree, outputDegree);
+			Assert.Equal(expectedCurve.Knots, outputKnots);
+			Assert.Equal(expectedCurve.ControlPoints, outputControlPts);
+		}
 	}
+
+
 }
---
+++ VerbNurbsSharp.XUnit/Core/ModifyTestCollection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using VerbNurbsSharp.Core;
+
+namespace VerbNurbsSharp.XUnit.Core
+{
+	public class ModifyTestCollection
+	{
+		public static IEnumerable<object[]> CurveKnotRefindData =>
+		new List<object[]>
+		{
+				new object[] {
+					3,
+					new KnotArray() { 0, 0, 0, 1, 2, 3, 4, 5, 6, 6, 6 },
+					new List<Vector>(){
+						new Vector(){ -11.652072, -3.739098, 0 },
+						new Vector(){ -4.034747, -0.191303, 0 },
+						new Vector(){ 1.669551, 0, 5.965165 },
+						new Vector(){ 5.843427, -9.895566, 0 },
+						new Vector(){ 8.556447, 0, -7.565151 },
+						new Vector(){ 18.608533, 0, 2.834758 }
+					},
+					new List<double>(){ 2.5 },
+					3,
+					new KnotArray() {0,0,0,1,2,2.5,3,4,5,6,6,6 },
+					new List<Vector>(){
+						new Vector(){ 0, 1, 3 },
+						new Vector(){ 0, 1, 3 },
+						new Vector(){ 0, 1, 3 },
+						new Vector(){ 0, 1, 3 },
+						new Vector(){ 0, 1, 3 }
+					}
+				}
+		};
+	}
+}
