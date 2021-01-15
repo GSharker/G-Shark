0c6ef62e2fd9643c594e202af7f962ae71804c80
Mirco Bianchini <sonomirco@gmail.com>
2021-01-15 16:35:37 +1100

    Basic function derivatives.

--- GeometrySharp/Evaluation/Eval.cs
+++ GeometrySharp/Evaluation/Eval.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
 using GeometrySharp.Geometry;
 
 namespace GeometrySharp.Evaluation
@@ -93,6 +94,171 @@ namespace GeometrySharp.Evaluation
             }
 
             return position;
+        }
+
+        public static Vector3 RationalCurveTanget(NurbsCurve curve, double t)
+        {
+            var derivs = RationalCurveDerivatives(curve, t, 1);
+            return derivs[1];
+        }
+
+        public static List<Vector3> RationalCurveDerivatives(NurbsCurve curve, double parameter, int numberDerivs = 1)
+        {
+            var derivatives = CurveDerivatives(curve, parameter, numberDerivs);
+            var Aders = Rational1d(derivatives);
+            var weightDerivatives = Weight1d(derivatives);
+            var CK = new List<Vector3>();
+
+            for (int k = 0; k < numberDerivs + 1; k++)
+            {
+                var v = Aders[k];
+
+                for (int i = 1; i < k + 1; i++)
+                {
+                    var valToMultiply = Binomial.Get(k, i) * weightDerivatives[i];
+                    var pt = CK[k - i];
+                    for (int j = 0; j < v.Count; j++)
+                    {
+                        v[j] = v[j] + valToMultiply * pt[j];
+                        v[j] = v[j] * 1 / weightDerivatives[i];
+                    }
+
+                    CK.Add(v);
+                }
+            }
+
+            return CK;
+        }
+
+        public static List<Vector3> CurveDerivatives(NurbsCurve curve, double parameter, int numberDerivs)
+        {
+            var degree = curve.Degree;
+            var controlPts = curve.ControlPoints;
+            var knots = curve.Knots;
+
+            if (!curve.Knots.AreValidRelations(degree, controlPts.Count))
+                throw new ArgumentException("Invalid relations between control points, knot");
+
+            var n = knots.Count - degree - 2;
+
+            var ptDimension = controlPts[0].Count;
+            var derivateOrder = numberDerivs < degree ? numberDerivs : degree;
+            var CK = Vector3.Zero2d(numberDerivs + 1, ptDimension);
+            var knotSpan = knots.Span(n, degree, parameter);
+            var derived2d = DerivativeBasisFunctionsGivenTwoN(knotSpan, parameter, degree, derivateOrder, knots);
+
+            for (int k = 0; k < derivateOrder + 1; k++)
+            {
+                for (int j = 0; j < degree + 1; j++)
+                {
+                    var valToMultiply = derived2d[k][j];
+                    var pt = controlPts[knotSpan - degree + j];
+                    for (int i = 0; i < CK[k].Count; i++)
+                        CK[k][i] = CK[k][i] + valToMultiply * pt[j];
+                }
+            }
+
+            return CK;
+        }
+
+        public static List<Vector3> DerivativeBasisFunctionsGivenTwoN(int span, double parameter, int degree,
+            int order, Knot knots)
+        {
+            var left = Sets.RepeatData(0.0, degree + 1);
+            var right = Sets.RepeatData(0.0, degree + 1);
+            // N[0][0] = 1.0 by definition
+            var ndu = Vector3.Zero2d(degree + 1, degree + 1);
+            ndu[0][0] = 1.0;
+
+            for (int j = 1; j < degree + 1; j++)
+            {
+                left[j] = parameter - knots[span + 1 - j];
+                right[j] = knots[span + j] - parameter;
+                var saved = 0.0;
+
+                for (int r = 0; r < j; r++)
+                {
+                    ndu[j][r] = right[r + 1] + left[j - r];
+                    var temp = ndu[r][j - 1] / ndu[j][r];
+
+                    ndu[r][j] = saved + right[r + 1] * temp;
+                    saved = left[j - r] * temp;
+                }
+
+                ndu[j][j] = saved;
+            }
+
+            // Load the basic functions.
+            var ders = Vector3.Zero2d(order + 1, degree + 1);
+            for (int j = 0; j < degree + 1; j++)
+                ders[0][j] = ndu[j][degree];
+
+            // Start calculating derivatives.
+            var a = Vector3.Zero2d(2, degree + 1);
+            // Loop over function index.
+            for (int r = 0; r < degree + 1; r++)
+            {
+                var s1 = 0;
+                var s2 = 0;
+                a[0][0] = 1.0;
+
+                for (int k = 1; k < order + 1; k++)
+                {
+                    var d = 0.0;
+                    var rk = r - k;
+                    var pk = degree - k;
+                    int j1, j2;
+
+                    if (r >= k)
+                    {
+                        a[s2][0] = a[s1][0] / ndu[pk + 1][rk];
+                        d = a[s2][0] * ndu[rk][pk];
+                    }
+
+                    if (rk >= -1)
+                        j1 = 1;
+                    else
+                        j1 = -rk;
+
+                    if (r - 1 <= pk)
+                        j2 = k - 1;
+                    else
+                        j2 = degree - r;
+
+                    for (int j = j1; j < j2 + 1; j++)
+                    {
+                        a[s2][j] = (a[s1][j] - a[s1][j - 1]) / ndu[pk + 1][rk + j];
+                        d += a[s2][j] * ndu[rk + j][pk];
+                    }
+
+                    if (r <= pk)
+                    {
+                        a[s2][k] = -a[s1][k - 1] / ndu[pk + 1][r];
+                        d += a[s2][k] * ndu[r][pk];
+                    }
+
+                    ders[k][r] = d;
+
+                    // Switch rows.
+                    var tempVal = s1;
+                    s1 = s2;
+                    s2 = tempVal;
+                }
+            }
+
+            // Multiply through by the the correct factors.
+            var acc = degree;
+            for (int k = 1; k < order + 1; k++)
+            {
+                for (int j = 0; j < degree + 1; j++)
+                {
+                    ders[k][j] *= acc;
+                }
+
+                acc *= degree - k;
+            }
+
+            return ders;
         }
     }
 }
