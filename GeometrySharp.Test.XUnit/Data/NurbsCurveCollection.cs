30ee2e68abadcf618a8b6bceb046894b20826b13
Mirco Bianchini <sonomirco@gmail.com>
2021-02-01 15:12:35 +1100

    Fixed naming.

--- GeometrySharp.Test.XUnit/Data/NurbsCurveCollection.cs
+++ GeometrySharp.Test.XUnit/Data/NurbsCurveCollection.cs
@@ -8,28 +8,28 @@ namespace GeometrySharp.Test.XUnit.Data
     {
         public static NurbsCurve NurbsCurveExample()
         {
-            int degree = 2;
-            List<Vector3> pts = new List<Vector3>()
+            var degree = 2;
+            var pts = new List<Vector3>()
             {
-                new Vector3(){-10,15,5},
-                new Vector3(){10,5,5},
-                new Vector3(){20,0,0}
+                new Vector3 {-10,15,5},
+                new Vector3 {10,5,5},
+                new Vector3 {20,0,0}
             };
-            Knot knots = new Knot() { 0, 0, 0, 1, 1, 1 };
+            var knots = new Knot { 0, 0, 0, 1, 1, 1 };
 
             return new NurbsCurve(degree, knots, pts);
         }
 
         public static NurbsCurve NurbsCurvePtsAndWeightsExample()
         {
-            int degree = 2;
-            List<Vector3> pts = new List<Vector3>()
+            var degree = 2;
+            var pts = new List<Vector3>()
             {
                 new Vector3(){-10,15,5},
                 new Vector3(){10,5,5},
                 new Vector3(){20,0,0}
             };
-            Knot knots = new Knot() { 1, 1, 1, 1, 1, 1 };
+            var knots = new Knot { 1, 1, 1, 1, 1, 1 };
             var weights = new List<double>() { 0.5, 0.5, 0.5 };
 
             return new NurbsCurve(degree, knots, pts, weights);
@@ -37,7 +37,7 @@ namespace GeometrySharp.Test.XUnit.Data
 
         public static NurbsCurve NurbsCurveExample2()
         {
-            var knots = new Knot() { 0.0, 0.0, 0.0, 0.0, 0.333333, 0.666667, 1.0, 1.0, 1.0, 1.0 };
+            var knots = new Knot { 0.0, 0.0, 0.0, 0.0, 0.333333, 0.666667, 1.0, 1.0, 1.0, 1.0 };
             var degree = 3;
             var controlPts = new List<Vector3>()
             {
