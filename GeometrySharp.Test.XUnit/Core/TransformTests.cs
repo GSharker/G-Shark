a37781988ee2aec98c916c3e5832683a7700efbe
Mirco Bianchini <sonomirco@gmail.com>
2021-02-15 11:54:20 +1100

    Added transfomation class and transformation class tests. Added constructor and tested. Added method identity and tested.

---
+++ GeometrySharp.Test.XUnit/Core/TransformTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using GeometrySharp.Core;
+using Xunit;
+
+namespace GeometrySharp.Test.XUnit.Core
+{
+    public class TransformTests
+    {
+        [Fact]
+        public void It_Returns_An_Instance_Of_Transform()
+        {
+            var transform = new Transform();
+
+            transform.Should().NotBeNull();
+            transform.Count.Should().Be(4);
+            transform[0].Count.Should().Be(4);
+        }
+
+        [Fact]
+        public void It_Returns_A_Identity_Transform_Matrix()
+        {
+            var transform = Transform.Identity();
+
+            transform.Count.Should().Be(4);
+            transform[0].Count.Should().Be(4);
+            transform[0][0].Should().Be(1);
+            transform[1][1].Should().Be(1);
+            transform[2][2].Should().Be(1);
+            transform[3][3].Should().Be(1);
+        }
+    }
+}
---
+++ GeometrySharp/Core/Transform.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometrySharp.Core
+{
+    public class Transform : List<IList<double>>
+    {
+        /// <summary>
+        /// Initializes a 4 x 4 transformation matrix.
+        /// All the values are set to zero.
+        /// </summary>
+        public Transform()
+        {
+            this.AddRange(Matrix.Construct(4,4));
+        }
+
+        /// <summary>
+        /// A new identity transformation matrix. An identity matrix defines no transformation.
+        /// The diagonal is (1,1,1,1)
+        /// </summary>
+        /// <returns>Gets the identity transformation matrix.</returns>
+        public static Transform Identity()
+        {
+            var transform = new Transform
+            {
+                [0] = { [0] = 1 },
+                [1] = { [1] = 1 },
+                [2] = { [2] = 1 },
+                [3] = { [3] = 1 },
+            };
+            return transform;
+        }
+    }
+}
