d9ccdc64f857f63e723f59bd50e5314e8ad0af80
Mirco Bianchini <sonomirco@gmail.com>
2021-02-22 20:17:34 +1100

    Added IEquatable for plane. Added toString. Added Transform.

--- GeometrySharp/Geometry/Plane.cs
+++ GeometrySharp/Geometry/Plane.cs
@@ -1,9 +1,13 @@
-﻿namespace GeometrySharp.Geometry
+﻿using System;
+using System.Text;
+using GeometrySharp.Core;
+
+namespace GeometrySharp.Geometry
 {
     /// <summary>
     /// A Plane is simply an origin point and normal.
     /// </summary>
-    public class Plane
+    public class Plane : IEquatable<Plane>
     {
         /// <summary>
         /// Construct a plane from a origin and a direction.
@@ -19,19 +23,39 @@
         }
 
         /// <summary>
-        /// Construct a plane from three points.
+        /// Construct a plane from three non-collinear points.
         /// </summary>
         /// <param name="pt1">Firs point representing the origin.</param>
         /// <param name="pt2">Second point representing the x direction.</param>
         /// <param name="pt3">Third point representing the y direction.</param>
         public Plane(Vector3 pt1, Vector3 pt2, Vector3 pt3)
         {
+            if(LinearAlgebra.Orientation(pt1, pt2, pt3) == 0)
+                throw new Exception("Plane cannot be created, the tree points must not be collinear");
+
+            Vector3 dir1 = pt2 - pt1;
+            Vector3 dir2 = pt3 - pt1;
+            Vector3 normal = Vector3.Cross(dir1, dir2);
+
             this.Origin = pt1;
-            var xDir = (pt2 - pt1).Unitize();
-            var yDir = (pt3 - pt1).Unitize();
-            this.XAxis = xDir;
-            this.YAxis = yDir;
-            this.ZAxis = Vector3.Cross(xDir, yDir).Unitize();
+            this.XAxis = dir1.Unitize();
+            this.YAxis = Vector3.Cross(normal, dir1).Unitize();
+            this.ZAxis = normal.Unitize();
+        }
+
+        /// <summary>
+        /// Construct a plane from an point and two directions.
+        /// </summary>
+        /// <param name="origin">Point representing the origin.</param>
+        /// <param name="xDirection">X direction.</param>
+        /// <param name="yDirection">Y direction.</param>
+        /// <param name="zDirection">Z direction.</param>
+        public Plane(Vector3 origin, Vector3 xDirection, Vector3 yDirection, Vector3 zDirection)
+        {
+            this.Origin = origin;
+            this.XAxis = xDirection.Unitize();
+            this.YAxis = yDirection.Unitize();
+            this.ZAxis = zDirection.Unitize();
         }
 
         /// <summary>
@@ -90,10 +114,113 @@
             return pt + projection;
         }
 
-        // Flip
-        // Rotate
-        // Align
-        // IEquatable
-        // ToString
+        /// <summary>
+        /// Swapping out the X and Y axes and inverting the Z axis.
+        /// </summary>
+        /// <returns>The flipped plane.</returns>
+        public Plane Flip()
+        {
+            Vector3 zDir = Vector3.Reverse(this.Normal);
+            return  new Plane(this.Origin, this.YAxis, this.XAxis, zDir);
+        }
+
+        // ToDo: add set origin.
+        // ToDo: add rotate.
+
+        /// <summary>
+        /// Transforms the plane by a transformation matrix.
+        /// </summary>
+        /// <param name="t">The transformation matrix to apply to the plane.</param>
+        /// <returns>The transformed plane.</returns>
+        public Plane Transform(Transform t)
+        {
+            Vector3 origin = this.Origin * t;
+
+            bool check = (Math.Abs(t[3][0]) <= GeoSharpMath.MAXTOLERANCE &&
+                          Math.Abs(t[3][1]) <= GeoSharpMath.MAXTOLERANCE &&
+                          Math.Abs(t[3][2]) <= GeoSharpMath.MAXTOLERANCE &&
+                          Math.Abs(1.0 - t[3][3]) <= GeoSharpMath.MAXTOLERANCE);
+
+            Vector3 xDir = check ? ((this.Origin + this.XAxis) * t) - origin : this.XAxis * t;
+            Vector3 yDir = check ? ((this.Origin + this.YAxis) * t) - origin : this.YAxis * t;
+            Vector3 zDir = check ? ((this.Origin + this.ZAxis) * t) - origin : this.ZAxis * t;
+
+            return new Plane(origin, xDir, yDir, zDir);
+        }
+
+        /// <summary>
+        /// Returns a boolean indicating whether the two given Planes are equal.
+        /// </summary>
+        /// <param name="plane1">The first Plane to compare.</param>
+        /// <param name="plane2">The second Plane to compare.</param>
+        /// <returns>True if the Planes are equal; False otherwise.</returns>
+        public static bool operator ==(Plane plane1, Plane plane2)
+        {
+            return Equals(plane1, plane2);
+        }
+
+        /// <summary>
+        /// Returns a boolean indicating whether the two given Planes are not equal.
+        /// </summary>
+        /// <param name="plane1">The first Plane to compare.</param>
+        /// <param name="plane2">The second Plane to compare.</param>
+        /// <returns>True if the Planes are not equal; False if they are equal.</returns>
+        public static bool operator !=(Plane plane1, Plane plane2)
+        {
+            return !Equals(plane1, plane2);
+        }
+
+        /// <summary>
+        /// Returns a boolean indicating whether the given Plane is equal to this Plane instance.
+        /// </summary>
+        /// <param name="other">The Plane to compare this instance to.</param>
+        /// <returns>True if the other Plane is equal to this instance; False otherwise.</returns>
+        public bool Equals(Plane other)
+        {
+            return this.Origin.Equals(other.Origin) &&
+                   this.XAxis.Equals(other.XAxis) &&
+                   this.YAxis.Equals(other.YAxis) &&
+                   this.ZAxis.Equals(other.ZAxis);
+        }
+
+        /// <summary>
+        /// Returns a boolean indicating whether the given Object is equal to this Plane instance.
+        /// </summary>
+        /// <param name="obj">The Object to compare against.</param>
+        /// <returns>True if the Object is equal to this Plane; False otherwise.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is Plane plane)
+            {
+                return Equals(plane);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>The hash code.</returns>
+        public override int GetHashCode()
+        {
+            return this.Origin.GetHashCode() + this.Normal.GetHashCode();
+        }
+
+        /// <summary>
+        /// Translate a plane into a readable format.
+        /// </summary>
+        /// <returns>The text format of a plane.</returns>
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine($"Origin({this.Origin})");
+            builder.AppendLine($"X({this.XAxis})");
+            builder.AppendLine($"Y({this.YAxis})");
+            builder.AppendLine($"Z({this.ZAxis})");
+
+            return builder.ToString();
+        }
     }
 }
