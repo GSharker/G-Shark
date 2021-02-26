21166c757af6d10cb46ac25c43005c80333192e9
Mirco Bianchini <sonomirco@gmail.com>
2021-02-26 19:41:54 +1100

    Added polyline pointAt and tested. Added RemapValue into Math, and tested indirectly inside pointAt.

--- GeometrySharp.Test.XUnit/Geometry/PolylineTests.cs
+++ GeometrySharp.Test.XUnit/Geometry/PolylineTests.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentAssertions;
+﻿using FluentAssertions;
 using GeometrySharp.Core;
 using GeometrySharp.Geometry;
+using System;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -68,10 +64,36 @@ namespace GeometrySharp.Test.XUnit.Geometry
         {
             Polyline polyline = new Polyline(ExamplePts);
 
-            var segments = polyline.Segments();
+            Line[] segments = polyline.Segments();
 
             segments.Length.Should().Be(3);
             segments[1].Length.Should().Be(segments[2].Length).And.BeApproximately(11.18034, GeoSharpMath.MAXTOLERANCE);
+        }
+
+        [Theory]
+        [InlineData(0.0, new double[]{ 5, 0, 0 })]
+        [InlineData(0.25, new double[] { 12.5, 11.25, 0 })]
+        [InlineData(0.55, new double[] { 18.25, 8.5, 0 })]
+        [InlineData(1.0, new double[] { 30, 10, 0 })]
+        public void It_Returns_A_Point_At_The_Given_Parameter(double t, double[] ptExpected)
+        {
+            Polyline polyline = new Polyline(ExamplePts);
+
+            Vector3 pt = polyline.PointAt(t);
+
+            pt.Should().BeEquivalentTo(new Vector3(ptExpected));
+        }
+
+        [Theory]
+        [InlineData(-0.1)]
+        [InlineData(1.05)]
+        public void PointAt_Throws_An_Exception_If_Parameter_Is_Smaller_Than_Zero_And_Bigger_Than_One(double t)
+        {
+            Polyline polyline = new Polyline(ExamplePts);
+
+            Func<Vector3> func = () => polyline.PointAt(t);
+
+            func.Should().Throw<Exception>();
         }
     }
 }
--- GeometrySharp/Geometry/Polyline.cs
+++ GeometrySharp/Geometry/Polyline.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using GeometrySharp.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
-using GeometrySharp.Core;
 
 namespace GeometrySharp.Geometry
 {
@@ -15,7 +14,6 @@ namespace GeometrySharp.Geometry
         // PointAt
         // TangentAt
         // ClosestPointTo
-        // Center
 
         /// <summary>
         /// Initializes a new empty polyline.
@@ -32,9 +30,11 @@ namespace GeometrySharp.Geometry
         public Polyline(IList<Vector3> vertices)
         {
             if(vertices.Count < 2)
+            {
                 throw new Exception("Insufficient points for a polyline.");
+            }
 
-            this.AddRange(CleanVerticesForShortLength(vertices));
+            AddRange(CleanVerticesForShortLength(vertices));
         }
 
         /// <summary>
@@ -45,8 +45,10 @@ namespace GeometrySharp.Geometry
         {
             double length = 0.0;
 
-            for (int i = 0; i < this.Count - 1; i++)
+            for (int i = 0; i < Count - 1; i++)
+            {
                 length += this[i].DistanceTo(this[i + 1]);
+            }
 
             return length;
         }
@@ -57,13 +59,48 @@ namespace GeometrySharp.Geometry
         /// <returns>A collection of lines.</returns>
         public Line[] Segments()
         {
-            int count = this.Count;
+            int count = Count;
             Line[] lines = new Line[count - 1];
 
             for (int i = 0; i < count - 1; i++)
+            {
                 lines[i] = new Line(this[i], this[i+1]);
+            }
 
             return lines;
+        }
+
+        /// <summary>
+        /// Get the point on the polyline at the give parameter.
+        /// The parameter must be between 0.0 and 1.0.
+        /// </summary>
+        /// <param name="t">Parameter to evaluate at.</param>
+        /// <returns>The point on the polyline at t.</returns>
+        public Vector3 PointAt(double t)
+        {
+            if (t < 0.0 || t > 1.0)
+            {
+                throw new Exception($"The value of t ({t}) must be between 0.0 and 1.0.");
+            }
+
+            int verticesCount = Count;
+            if (t <= GeoSharpMath.EPSILON)
+            {
+                return this[0];
+            }
+
+            if (Math.Abs(t - 1) <= GeoSharpMath.EPSILON)
+            {
+                return this[verticesCount - 1];
+            }
+
+            double tRemapped = GeoSharpMath.RemapValue(t, new Interval(0.0, 1.0), new Interval(0.0, verticesCount - 1));
+
+            double floorValue = Math.Floor(tRemapped);
+            int segmentIndex = (int)floorValue;
+            tRemapped -= floorValue;
+
+            return this[segmentIndex] * (1 - tRemapped) + this[segmentIndex + 1] * tRemapped;
         }
 
         /// <summary>
@@ -82,18 +119,27 @@ namespace GeometrySharp.Geometry
             {
                 flag[i] = 0;
                 if (vertices[i - 1].DistanceTo(vertices[i]) <= GeoSharpMath.MAXTOLERANCE)
+                {
                     flag[i] = 1;
+                }
             }
 
             int numberOfCoincidence = flag.Sum();
-            if (numberOfCoincidence == 0) return vertices;
+            if (numberOfCoincidence == 0)
+            {
+                return vertices;
+            }
 
             Vector3[] cleanedList = new Vector3[verticesCount - numberOfCoincidence];
 
             int counter = 0;
             for (int i = 0; i < vertices.Count; i++)
             {
-                if (flag[i] != 0) continue;
+                if (flag[i] != 0)
+                {
+                    continue;
+                }
+
                 cleanedList[counter] = vertices[i];
                 counter++;
             }
