21de8bc9ed0a294e56fc8a4d8a28d4f210d48bcb
Mirco Bianchini <sonomirco@gmail.com>
2021-03-15 08:10:59 +1100

    Added linePlane intersection and tested.

--- GeometrySharp.Test.XUnit/Operation/IntersectionTests.cs
+++ GeometrySharp.Test.XUnit/Operation/IntersectionTests.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FluentAssertions;
 using GeometrySharp.Geometry;
 using GeometrySharp.Operation;
@@ -26,14 +22,53 @@ namespace GeometrySharp.Test.XUnit.Operation
             Plane pl1 = Plane.PlaneYZ.SetOrigin(new Vector3{10,10,5});
             Plane pl2 = Plane.PlaneXZ.SetOrigin(new Vector3 {10, -10, -5});
 
-            Ray intersection0 = Intersect.PlaneToPlane(pl0, pl1);
-            Ray intersection1 = Intersect.PlaneToPlane(pl1, pl2);
+            Ray intersection0 = Intersect.PlanePlane(pl0, pl1);
+            Ray intersection1 = Intersect.PlanePlane(pl1, pl2);
 
             intersection0.Position.Should().BeEquivalentTo(new Vector3 {10, 0, 0});
             intersection0.Direction.Should().BeEquivalentTo(new Vector3 { 0, 1, 0 });
 
             intersection1.Position.Should().BeEquivalentTo(new Vector3 { 10, -10, 0 });
             intersection1.Direction.Should().BeEquivalentTo(new Vector3 { 0, 0, 1 });
+        }
+
+        [Fact]
+        public void PlaneToPlane_Throws_An_Exception_If_Planes_Are_Parallel()
+        {
+            Plane pl0 = Plane.PlaneXY;
+            Plane pl1 = Plane.PlaneXY.SetOrigin(new Vector3 { 10, 10, 5 });
+
+            Func<object> func = () => Intersect.PlanePlane(pl0, pl1);
+
+            func.Should().Throw<Exception>().WithMessage("The two planes are parallel.");
+        }
+
+        [Fact]
+        public void It_Returns_The_Intersection_Point_Between_A_Segment_And_A_Plane()
+        {
+            Plane pl = Plane.PlaneYZ.SetOrigin(new Vector3 { 10, 20, 5 });
+            Line ln0 = new Line(new Vector3 { 0, 0, 0 }, new Vector3 { 20, 20, 10 });
+            Line ln1 = new Line(new Vector3 { 0, 0, 0 }, new Vector3 { 5, 5, 10 });
+
+            Vector3 pt0 = Intersect.LinePlane(ln0, pl);
+            Vector3 pt1 = Intersect.LinePlane(ln1, pl);
+
+            pt0.Equals(new Vector3 {10, 10, 5}).Should().BeTrue();
+            pt1.Equals(new Vector3 { 10, 10, 20 }).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(new double[] { 0, 0, 0 }, new double[] { 0, 10, 0 })]
+        [InlineData(new double[] { 0, 0, 0 }, new double[] { 0, 0, 10 })]
+        [InlineData(new double[] { 10, 20, 5 }, new double[] { 10, 20, 10 })]
+        public void LinePlane_Throws_An_Exception_If_Line_Is_Parallel(double[] startPt, double[] endPt)
+        {
+            Plane pl = Plane.PlaneYZ.SetOrigin(new Vector3 { 10, 20, 5 });
+            Line ln = new Line(new Vector3 (startPt), new Vector3(endPt));
+
+            Func<object> func = () => Intersect.LinePlane(ln, pl);
+
+            func.Should().Throw<Exception>().WithMessage("Segment parallel to the plane or lies in plane.");
         }
     }
 }
--- GeometrySharp/Operation/Intersect.cs
+++ GeometrySharp/Operation/Intersect.cs
@@ -7,7 +7,7 @@ namespace GeometrySharp.Operation
     /// <summary>
     /// Intersection provides various tools for all kinds of intersection
     /// </summary>
-    public static class Intersect
+    public class Intersect
     {
         // ToDo: Curve-Plane
         // ToDo: Curve-Curve
@@ -16,7 +16,6 @@ namespace GeometrySharp.Operation
         // ToDo: Polyline-Plane
         // ToDo: Polyline-Polyline
         // ToDo: Polyline-Self
-        // ToDo: Plane-Plane-Plane
         // ToDo: Line-Plane
         // ToDo: Line-Line
 
@@ -26,7 +25,7 @@ namespace GeometrySharp.Operation
         /// <param name="p1">The first plane.</param>
         /// <param name="p2">The second plane.</param>
         /// <returns>The intersection as <see cref="Ray"/>.</returns>
-        public static Ray PlaneToPlane(Plane p1, Plane p2)
+        public static Ray PlanePlane(Plane p1, Plane p2)
         {
             Vector3 plNormal1 = p1.Normal;
             Vector3 plNormal2 = p2.Normal;
@@ -37,33 +36,32 @@ namespace GeometrySharp.Operation
 
             // Find the largest index of directionVec of the line of intersection.
 
-            int largIndex = 0;
-            double mI = Math.Abs(directionVec[0]);
-            double m1 = Math.Abs(directionVec[1]);
-            double m2 = Math.Abs(directionVec[2]);
+            int largeIndex = 0;
+            double ai = Math.Abs(directionVec[0]);
+            double ay = Math.Abs(directionVec[1]);
+            double az = Math.Abs(directionVec[2]);
 
-            if (m1 > m2)
+            if (ay > ai)
             {
-                largIndex = 1;
-                mI = m1;
+                largeIndex = 1;
+                ai = ay;
             }
 
-            if (m2 > m1)
+            if (az > ai)
             {
-                largIndex = 2;
-                mI = m2;
+                largeIndex = 2;
             }
 
             double a1, b1, a2, b2;
 
-            if (largIndex == 0)
+            if (largeIndex == 0)
             {
                 a1 = plNormal1[1];
                 b1 = plNormal1[2];
                 a2 = plNormal2[1];
                 b2 = plNormal2[2];
             }
-            else if (largIndex == 1)
+            else if (largeIndex == 1)
             {
                 a1 = plNormal1[0];
                 b1 = plNormal1[2];
@@ -88,14 +86,38 @@ namespace GeometrySharp.Operation
 
             Vector3 pt = new Vector3();
 
-            if (largIndex == 0)
+            if (largeIndex == 0)
                 pt.AddRange(new[] { 0.0, corX, corY });
-            else if (largIndex == 1)
+            else if (largeIndex == 1)
                 pt.AddRange(new[] { corX, 0.0, corY });
             else
                 pt.AddRange(new[] { corX, corY, 0.0 });
 
             return new Ray(pt, directionVec.Unitize());
+        }
+
+        /// <summary>
+        /// Find the point intersection of a line and a plane.
+        /// http://geomalgorithms.com/a05-_intersect-1.html
+        /// </summary>
+        /// <param name="line">The segment to intersect.</param>
+        /// <param name="plane">The plane has to be intersected.</param>
+        /// <returns>The point representing the unique intersection.</returns>
+        public static Vector3 LinePlane(Line line, Plane plane)
+        {
+            Vector3 lnDir = line.Direction;
+            Vector3 ptPlane = plane.Origin - line.Start;
+
+            double denominator = Vector3.Dot(plane.Normal, lnDir);
+            double numeric = Vector3.Dot(plane.Normal, ptPlane);
+
+            if (Math.Abs(denominator) < GeoSharpMath.EPSILON)
+                throw new Exception("Segment parallel to the plane or lies in plane.");
+
+            // Compute the intersect parameter.
+            double t = numeric / denominator;
+
+            return line.Start + lnDir * t;
         }
     }
 }
