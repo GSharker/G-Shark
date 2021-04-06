6a1b82ebce783d19ba9a01497e84567785d062d0
Mirco Bianchini <sonomirco@gmail.com>
2021-04-07 08:02:33 +1000

    Clean and renamed files. Added comments.

--- GeometrySharp.Test.XUnit/VerbTests.cs
+++ GeometrySharp.Test.XUnit/VerbTests.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using verb.core;
+﻿using verb.core;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace GeometrySharp.Test.XUnit
 {
+    /// <summary>
+    /// Test used to review methods and functionality used in verb.
+    /// Once the library will be completed this test will be removed.
+    /// </summary>
     public class VerbTests
     {
         private readonly ITestOutputHelper _testOutput;
@@ -21,19 +20,19 @@ namespace GeometrySharp.Test.XUnit
         [Fact]
         public void rationalBezierCurveArcLength()
         {
-            var pts = new Array<object>();
+            Array<object> pts = new Array<object>();
 
             pts.push(new Array<double>(new double[] { 0, 0, 0, 1 }));
             pts.push(new Array<double>(new double[] { 0.5, 0, 0, 1 }));
             pts.push(new Array<double>(new double[] { 2.5, 0, 0, 1 }));
             pts.push(new Array<double>(new double[] { 3, 0, 0, 1 }));
 
-            var knots = new Array<double>(new double[] { 0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 2.0, 2.0 });
-            var weights = new Array<double>(new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 });
+            Array<double> knots = new Array<double>(new double[] { 0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 2.0, 2.0 });
+            Array<double> weights = new Array<double>(new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 });
 
-            var curve = verb.geom.NurbsCurve.byPoints(pts, 3);
+            verb.geom.NurbsCurve curve = verb.geom.NurbsCurve.byPoints(pts, 3);
 
-            var res = verb.eval.Analyze.rationalBezierCurveArcLength(curve._data, 1, 16);
+            double res = verb.eval.Analyze.rationalBezierCurveArcLength(curve._data, 1, 16);
 
             _testOutput.WriteLine($"{res}");
         }
@@ -41,7 +40,7 @@ namespace GeometrySharp.Test.XUnit
         [Fact]
         public void NurbsCurvebyPoints()
         {
-            var pts = new Array<object>();
+            Array<object> pts = new Array<object>();
 
             pts.push(new Array<double>(new double[] { 5, 5, 0 }));
             pts.push(new Array<double>(new double[] { 10, 10, 0 }));
@@ -50,15 +49,15 @@ namespace GeometrySharp.Test.XUnit
             pts.push(new Array<double>(new double[] { 45, 10, 0 }));
             pts.push(new Array<double>(new double[] { 50, 5, 0 }));
 
-            var knots = new Array<double>(new double[] { 0.0, 0.0, 0.0, 0.0, 0.333333, 0.666667, 1.0, 1.0, 1.0, 1.0 });
-            var weights = new Array<double>(new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 });
+            Array<double> knots = new Array<double>(new double[] { 0.0, 0.0, 0.0, 0.0, 0.333333, 0.666667, 1.0, 1.0, 1.0, 1.0 });
+            Array<double> weights = new Array<double>(new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 });
 
-            var curve = verb.geom.NurbsCurve.byKnotsControlPointsWeights(3, knots, pts, weights);
-            var curve2 = verb.geom.NurbsCurve.byPoints(pts, 3);
+            verb.geom.NurbsCurve curve = verb.geom.NurbsCurve.byKnotsControlPointsWeights(3, knots, pts, weights);
+            verb.geom.NurbsCurve curve2 = verb.geom.NurbsCurve.byPoints(pts, 3);
 
-            var ptAt = curve2.point(0.5);
-            var vec = verb.core.Vec.normalized(curve2.tangent(0.5));
-            var k = curve2.knots();
+            Array<double> ptAt = curve2.point(0.5);
+            Array<double> vec = verb.core.Vec.normalized(curve2.tangent(0.5));
+            Array<double> k = curve2.knots();
 
             for (int i = 0; i < k.length; i++)
             {
@@ -68,7 +67,7 @@ namespace GeometrySharp.Test.XUnit
             _testOutput.WriteLine($"{k}");
             _testOutput.WriteLine($"{ptAt[0]},{ptAt[1]},{ptAt[2]}");
 
-            var lazy = new verb.core.LazyCurveBoundingBoxTree(curve._data, -10);
+            LazyCurveBoundingBoxTree lazy = new verb.core.LazyCurveBoundingBoxTree(curve._data, -10);
 
             _testOutput.WriteLine($"{lazy._knotTol}");
             _testOutput.WriteLine($"{curve._data.knots}");
@@ -77,8 +76,8 @@ namespace GeometrySharp.Test.XUnit
         [Fact]
         public void CurvesIntersections()
         {
-            var pts0 = new Array<object>();
-            var pts1 = new Array<object>();
+            Array<object> pts0 = new Array<object>();
+            Array<object> pts1 = new Array<object>();
 
             pts0.push(new Array<double>(new double[] { -5,0,0 }));
             pts0.push(new Array<double>(new double[] { 10, 0, 0 }));
@@ -92,13 +91,13 @@ namespace GeometrySharp.Test.XUnit
             pts1.push(new Array<double>(new double[] { 3, 10, 0 }));
             pts1.push(new Array<double>(new double[] { 5, 12, 0 }));
 
-            var knots = new Array<double>(new double[] { 0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0 });
-            var weights = new Array<double>(new double[] { 1, 1, 1, 1, 1 });
+            Array<double> knots = new Array<double>(new double[] { 0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0 });
+            Array<double> weights = new Array<double>(new double[] { 1, 1, 1, 1, 1 });
 
-            var curve0 = verb.geom.NurbsCurve.byKnotsControlPointsWeights(3, knots, pts0, weights);
-            var curve1 = verb.geom.NurbsCurve.byKnotsControlPointsWeights(3, knots, pts1, weights);
+            verb.geom.NurbsCurve curve0 = verb.geom.NurbsCurve.byKnotsControlPointsWeights(3, knots, pts0, weights);
+            verb.geom.NurbsCurve curve1 = verb.geom.NurbsCurve.byKnotsControlPointsWeights(3, knots, pts1, weights);
 
-            var intersections = verb.eval.Intersect.curves(curve0._data, curve1._data, verb.core.Constants.TOLERANCE);
+            Array<object> intersections = verb.eval.Intersect.curves(curve0._data, curve1._data, verb.core.Constants.TOLERANCE);
             
             _testOutput.WriteLine(intersections.length.ToString());
             for (int i = 0; i < intersections.length; i++)
@@ -112,7 +111,7 @@ namespace GeometrySharp.Test.XUnit
         [Fact]
         public void rationalCurveAdaptiveSample()
         {
-            var pts = new Array<object>();
+            Array<object> pts = new Array<object>();
 
             pts.push(new Array<double>(new double[] { 5, 5, 0 }));
             pts.push(new Array<double>(new double[] { 10, 10, 0 }));
@@ -121,14 +120,14 @@ namespace GeometrySharp.Test.XUnit
             pts.push(new Array<double>(new double[] { 45, 10, 0 }));
             pts.push(new Array<double>(new double[] { 50, 5, 0 }));
 
-            var knots = new Array<double>(new double[] { 0.0, 0.0, 0.0, 0.0, 0.333333, 0.666667, 1.0, 1.0, 1.0, 1.0 });
-            var knots2 = new Array<double>(new double[] { 0.0, 0.2, 0.4, 0.6, 0.8, 1.0});
-            var weights = new Array<double>(new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 });
+            Array<double> knots = new Array<double>(new double[] { 0.0, 0.0, 0.0, 0.0, 0.333333, 0.666667, 1.0, 1.0, 1.0, 1.0 });
+            Array<double> knots2 = new Array<double>(new double[] { 0.0, 0.2, 0.4, 0.6, 0.8, 1.0});
+            Array<double> weights = new Array<double>(new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 });
 
-            var curve = verb.geom.NurbsCurve.byKnotsControlPointsWeights(3, knots, pts, weights);
-            var curve2 = verb.geom.NurbsCurve.byPoints(pts, 1);
+            verb.geom.NurbsCurve curve = verb.geom.NurbsCurve.byKnotsControlPointsWeights(3, knots, pts, weights);
+            verb.geom.NurbsCurve curve2 = verb.geom.NurbsCurve.byPoints(pts, 1);
 
-            var p = verb.eval.Tess.rationalCurveAdaptiveSample(curve._data, 1e-6, null);
+            Array<object> p = verb.eval.Tess.rationalCurveAdaptiveSample(curve._data, 1e-6, null);
 
             _testOutput.WriteLine($"{p.length}");
             for (int i = 0; i < p.length; i++)
@@ -140,17 +139,17 @@ namespace GeometrySharp.Test.XUnit
         [Fact]
         public void rationalCurveRegularSample()
         {
-            var pts = new Array<object>();
+            Array<object> pts = new Array<object>();
 
             pts.push(new Array<double>(new double[] { 1, 0, 0, 1 }));
             pts.push(new Array<double>(new double[] { 1, 1, 0, 1 }));
             pts.push(new Array<double>(new double[] { 0, 2, 0, 1 }));
 
-            var knots = new Array<double>(new double[] { 0, 0, 0, 1, 1, 1 });
+            Array<double> knots = new Array<double>(new double[] { 0, 0, 0, 1, 1, 1 });
 
-            var curve = new verb.core.NurbsCurveData(2, knots, pts);
+            NurbsCurveData curve = new verb.core.NurbsCurveData(2, knots, pts);
 
-            var p = verb.eval.Tess.rationalCurveRegularSample(curve, 10, true);
+            Array<object> p = verb.eval.Tess.rationalCurveRegularSample(curve, 10, true);
 
             _testOutput.WriteLine($"{p.length}");
             for (int i = 0; i < p.length; i++)
@@ -162,7 +161,7 @@ namespace GeometrySharp.Test.XUnit
         [Fact]
         public void decomposeMatrix()
         {
-            var matrix = new Array<object> ();
+            Array<object> matrix = new Array<object> ();
 
             //matrix.push(new Array<object>(new object[] {10, -7, 0}));
             //matrix.push(new Array<object>(new object[] { -3, 2, 6 }));
@@ -176,12 +175,12 @@ namespace GeometrySharp.Test.XUnit
             //matrix.push(new Array<object>(new object[] { 6,3 }));
 
 
-            var matrixLU = Mat.LU(matrix);
+            verb.core._Mat.LUDecomp matrixLU = Mat.LU(matrix);
 
             //var vector = new Array<double>(new double[] {3,13,4});
-            var vector = new Array<double>(new double[] { 0,4,17});
+            Array<double> vector = new Array<double>(new double[] { 0,4,17});
 
-            var solved = Mat.LUsolve(matrixLU, vector);
+            Array<double> solved = Mat.LUsolve(matrixLU, vector);
 
             for (int i = 0; i < matrixLU.LU.length; i++)
             {
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # [GeometrySharp](https://github.com/cesarecaoduro/GeometrySharp/tree/master/GeometrySharp "GeometrySharp")
 This repository is inspired by the library **Verb** --> [https://github.com/pboyer/verb](https://github.com/pboyer/verb). 
 The initial library uses Haxe to build for different languages including C# with some limitation and bugs. This repository is a porting of the library in native C# and is based on NET5 framework.
-Still in the very early stages but I am planning to include few examples, including a ConsoleApp and a SPA based on Blazor WebAssembly.
+Still in the very early stages but we are planning to include few examples, including a ConsoleApp and a SPA based on Blazor WebAssembly.
 # [GeometrySharp.Test.XUnit](https://github.com/cesarecaoduro/GeometrySharp/tree/master/GeometrySharp.Test.XUnit "GeometrySharp.Test.XUnit")
-This projects implements unit tests using XUnit and Fluent Assertion
+This projects implements unit tests using XUnit and Fluent Assertion.
