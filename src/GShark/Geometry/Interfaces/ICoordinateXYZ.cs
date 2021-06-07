cdc99940064cd86d290905e3166629b687399ebe
Mirco Bianchini <sonomirco@gmail.com>
2021-06-07 16:10:14 +1000

    Renamed Point4 in ControlPoint. Started to move methods between Vector3 to Point3.

--- src/GShark/Geometry/Interfaces/ICoordinateXYZ.cs
+++ src/GShark/Geometry/Interfaces/ICoordinateXYZ.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace GShark.Geometry.Interfaces
 {
+    /// <summary>
+    /// This interface defines the basic coordinates used to describes a geometry in a 3-dimensional space.
+    /// </summary>
     public interface ICoordinateXYZ
     {
-        public double X { get; set; }
-        public double Y { get; set; }
-        public double Z { get; set; }
+        /// <summary>
+        /// Gets the X coordinate of the Euclidean 3d geometry.
+        /// </summary>
+        public double X { get; }
+
+        /// <summary>
+        /// Gets the Y coordinate of the Euclidean 3d geometry.
+        /// </summary>
+        public double Y { get; }
+
+        /// <summary>
+        /// Gets the Z coordinate of the Euclidean 3d geometry.
+        /// </summary>
+        public double Z { get; }
     }
 }
--- src/GShark/Geometry/V3.cs
+++ src/GShark/Geometry/V3.cs
@@ -1,5 +1,6 @@
 ﻿using GShark.Geometry.Interfaces;
 using System;
+using GShark.Core;
 
 namespace GShark.Geometry
 {
@@ -8,9 +9,75 @@ namespace GShark.Geometry
     /// </summary>
     public struct V3 : ICoordinateXYZ, IComparable<V3>, IEquatable<V3>
     {
-        public double X { get; set; }
-        public double Y { get; set; }
-        public double Z { get; set; }
+        /// <summary>
+        /// Creates the instance of a vector, by three components.
+        /// </summary>
+        /// <param name="x">The first component.</param>
+        /// <param name="y">The second component.</param>
+        /// <param name="z">The third component.</param>
+        public V3(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        /// <summary>
+        /// Creates the instance of a vector, coping the coordinate of a points.
+        /// </summary>
+        /// <param name="point">The point used to create the vector.</param>
+        public V3(Point3 point)
+        {
+            X = point.X;
+            Y = point.Y;
+            Z = point.Z;
+        }
+
+        /// <summary>
+        /// Creates the instance of a vector, coping the components from another vector.
+        /// </summary>
+        /// <param name="vector">The vector used to copied the components.</param>
+        public V3(V3 vector)
+        {
+            X = vector.X;
+            Y = vector.Y;
+            Z = vector.Z;
+        }
+
+        /// <summary>
+        /// Gets the value of a point at location GeoSharpMath.UNSET_VALUE,GeoSharpMath.UNSET_VALUE,GeoSharpMath.UNSET_VALUE.
+        /// </summary>
+        public static V3 Unset => new V3(GeoSharpMath.UNSET_VALUE, GeoSharpMath.UNSET_VALUE, GeoSharpMath.UNSET_VALUE);
+
+        /// <summary>
+        /// Gets the value of the vector with components 1,0,0.
+        /// </summary>
+        public static V3 XAxis => new V3(1.0, 0.0, 0.0 );
+
+        /// <summary>
+        /// Gets the value of the vector with components 0,1,0.
+        /// </summary>
+        public static V3 YAxis => new V3( 0.0, 1.0, 0.0 );
+
+        /// <summary>
+        /// Gets the value of the vector with components 0,0,1.
+        /// </summary>
+        public static V3 ZAxis => new V3( 0.0, 0.0, 1.0 );
+
+        /// <summary>
+        /// Gets the X coordinate of the vector.
+        /// </summary>
+        public double X { get; }
+
+        /// <summary>
+        /// Gets the Y coordinate of the vector.
+        /// </summary>
+        public double Y { get; }
+
+        /// <summary>
+        /// Gets the Z coordinate of the vector.
+        /// </summary>
+        public double Z { get; }
 
         public int CompareTo(V3 other)
         {
