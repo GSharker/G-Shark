5a00ca7799d62bcd337846af15b35518cac041cd
Mirco Bianchini <sonomirco@gmail.com>
2021-07-05 15:30:34 +1000

    Fixed some typos.

--- src/GShark/Geometry/Arc.cs
+++ src/GShark/Geometry/Arc.cs
@@ -234,7 +234,7 @@ namespace GShark.Geometry
             double twoPi = 2.0 * Math.PI;
 
             (double u, double v) = Plane.ClosestParameters(pt);
-            if (Math.Abs(u) < GGeoSharkMathMaxTolerance && Math.Abs(v) < GGeoSharkMathMaxTolerance)
+            if (Math.Abs(u) < GeoSharkMath.MaxTolerance && Math.Abs(v) < GeoSharkMath.MaxTolerance)
             {
                 return PointAt(0.0);
             }
@@ -404,8 +404,8 @@ namespace GShark.Geometry
                 return false;
             }
 
-            return Math.Abs(Radius - other.Radius) < GGeoSharkMathMaxTolerance &&
-                   Math.Abs(Angle - other.Angle) < GGeoSharkMathMaxTolerance &&
+            return Math.Abs(Radius - other.Radius) < GeoSharkMath.MaxTolerance &&
+                   Math.Abs(Angle - other.Angle) < GeoSharkMath.MaxTolerance &&
                    Plane == other.Plane;
         }
 
@@ -424,7 +424,7 @@ namespace GShark.Geometry
         /// <returns>Text value.</returns>
         public override string ToString()
         {
-            return $"Arc(R:{Radius} - A:{GGeoSharkMathToDegrees(Angle)})";
+            return $"Arc(R:{Radius} - A:{GeoSharkMath.ToDegrees(Angle)})";
         }
 
 
