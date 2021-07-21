c22fbcd5436a53db7a8c6fa38ddf6ba3788d9192
Guido Maciocci <guido.maciocci@lendlease.com>
2021-07-21 11:48:15 +1000

    Final implementation and tests.

--- src/GShark/Operation/Divide.cs
+++ src/GShark/Operation/Divide.cs
@@ -46,12 +46,13 @@ namespace GShark.Operation
         /// <param name="curve">The curve object to divide.</param>
         /// <param name="divisions">The number of parts to split the curve into.</param>
         /// <returns>A tuple define the t values where the curve is divided and the lengths between each division.</returns>
-        public static (List<double> tValues, List<double> lengths) CurveByCount(ICurve curve, int divisions)
+        public static List<double> CurveByCount(ICurve curve, int divisions)
         {
             double approximatedLength = Analyze.CurveLength(curve);
             double arcLengthSeparation = approximatedLength / divisions;
-
-            return CurveByLength(curve, arcLengthSeparation);
+            var divisionByLength = CurveByLength(curve, arcLengthSeparation);
+            var tValues = divisionByLength.tValues;
+            return tValues;
         }
 
         /// <summary>
@@ -101,39 +102,62 @@ namespace GShark.Operation
         }
 
         /// <summary>
-        /// Creates rotation minimized perpendicular frames at given t parameters along the curve.
+        /// Creates rotation minimized perpendicular frames (RMF) at given t parameters along the curve.
+        /// <para>//Double reflection method taken from Wang, W., J¨uttler, B., Zheng, D., and Liu, Y. 2008. "Computation of rotation minimizing frame."</para>
         /// </summary>
-        public static List<Plane> PerpendicularFrames(ICurve curve, List<double> tValues)
+        ///<param name="curve">The input curve.</param>
+        /// ///<param name="uValues">The curve parameter values to locate perpendicular curve frames</param>
+        public static List<Plane> PerpendicularFrames(ICurve curve, List<double> uValues)
         {
-            var pointsOnCurve = tValues.Select(t => curve.PointAt(t)).ToList();
-            var pointsOnCurveTan = tValues.Select(t => Evaluation.RationalCurveTangent(curve, t)).ToList();
-            var firstParameter = tValues[0];
+            var pointsOnCurve = uValues.Select(curve.PointAt).ToList(); //get points at t values
+            var pointsOnCurveTan = uValues.Select(t => Evaluation.RationalCurveTangent(curve, t)).ToList(); //get tangents at t values
+            var firstParameter = uValues[0]; //get first t value
+            
+            //Create initial frame at first parameter
             var origin = curve.PointAt(firstParameter);
             var crvTan = Evaluation.RationalCurveTangent(curve, firstParameter);
             var crvNormal = Vector3.PerpendicularTo(crvTan);
             var yAxis = Vector3.CrossProduct(crvTan, crvNormal);
             var xAxis = Vector3.CrossProduct(yAxis, crvTan);
-            Plane[] output = new Plane[pointsOnCurve.Count];
-            output[0] = new Plane(origin, xAxis, yAxis);
+            
+            //Set initial frame
+            Plane[] perpFrames = new Plane[pointsOnCurve.Count];
+            perpFrames[0] = new Plane(origin, xAxis, yAxis);
 
-            //Wang, W., J¨uttler, B., Zheng, D., and Liu, Y. 2008. "Computation of rotation minimizing frame."
+            //Given boundary data(x0, t0; x1, t1) and an initial right-handed
+            //orthonormal frame U0 = (r0, s0, t0) at x0, the next frame U1 = (r1, s1, t1)
+            //at x1 for RMF approximation is computed by the double reflection method in
+            //the following two steps.
+            //
+            //Step 1.Let R1 denote the reflection in the bisecting plane of the points x0
+            //and x1(see Figure 4).Use R1 to map U0 to a left - handed orthonormal frame
+            //UL0 = (rL0, sL0, tL0).
+            //
+            //Step 2.Let R2 denote the reflection in the bisecting plane of the points x1 + tL
+            //0 and x1 +t1. Use R2 to map UL0 to a right - handed orthonormal frame U1 = (r1, s1, t1).
+            //Output U1.
+
             for (int i = 0; i < pointsOnCurve.Count - 1; i++)
             {
-                Vector3 v1 = pointsOnCurve[i + 1] - pointsOnCurve[i];
+                Vector3 v1 = pointsOnCurve[i + 1] - pointsOnCurve[i]; //compute reflection vector of R1
                 double c1 = v1 * v1;
-                Vector3 rLi = output[i].XAxis - (2 / c1) * (v1 * output[i].XAxis) * v1;
-                Vector3 tLi = pointsOnCurveTan[i] - (2 / c1) * (v1 * pointsOnCurveTan[i]) * v1;
-                Vector3 v2 = pointsOnCurveTan[i + 1] - tLi;
+                Vector3 rLi = perpFrames[i].XAxis - (2 / c1) * (v1 * perpFrames[i].XAxis) * v1; //compute reflected rL vector by R1
+                Vector3 tLi = pointsOnCurveTan[i] - (2 / c1) * (v1 * pointsOnCurveTan[i]) * v1; //compute reflected tL vector by R1
+                Vector3 v2 = pointsOnCurveTan[i + 1] - tLi; //compute reflection vector of R2
                 double c2 = v2 * v2;
-                Vector3 rINext = rLi - (2 / c2) * (v2 * rLi) * v2;
-                var sINext = Vector3.CrossProduct(pointsOnCurveTan[i + 1], rINext);
-                var pln = new Plane();
-                pln.Origin = pointsOnCurve[i + 1];
-                pln.XAxis = rINext;
-                pln.YAxis = sINext;
-                pln.ZAxis = pointsOnCurveTan[i + 1];
-                output[i + 1] = pln;
+                Vector3 rNext = rLi - (2 / c2) * (v2 * rLi) * v2; //compute reflected r vector by R2
+                var sNext = Vector3.CrossProduct(pointsOnCurveTan[i + 1], rNext); //compute vector s[i+1] of next frame
+                
+                //create output frame
+                var frameNext = new Plane();
+                frameNext.Origin = pointsOnCurve[i + 1];
+                frameNext.XAxis = rNext;
+                frameNext.YAxis = sNext;
+                frameNext.ZAxis = pointsOnCurveTan[i + 1];
+                perpFrames[i + 1] = frameNext; //output frame
             }
+
+            return perpFrames.ToList();
         }
     }
 }
