eca24163dd54b1dee01fe4cd6b0f33e1ae639ca1
Mirco Bianchini <sonomirco@gmail.com>
2021-07-27 10:54:16 +1000

    Added enum surface direction.

---
+++ src/GShark/Geometry/Enum/SurfaceDirection.cs
@@ -0,0 +1,25 @@
+﻿namespace GShark.Geometry.Enum
+{
+    /// <summary>
+    /// Enum defining the direction of a surface.
+    /// </summary>
+    public enum SurfaceDirection
+    {
+        Unknown = 0,
+
+        /// <summary>
+        /// The U direction of a surface.
+        /// </summary>
+        UDirection, 
+
+        /// <summary>
+        /// The V direction fo a surface.
+        /// </summary>
+        VDirection,
+
+        /// <summary>
+        /// The normal direction of a surface.
+        /// </summary>
+        Normal
+    }
+}
