bbdc47410e1e3b2027856b87d54443e9659158de
Mirco Bianchini <sonomirco@gmail.com>
2021-08-11 18:12:06 +1000

    Finish to write elevation degree.

--- src/GShark/Operation/Modify.cs
+++ src/GShark/Operation/Modify.cs
@@ -155,5 +155,230 @@ namespace GShark.Operation
 
             return new NurbsCurve(curve.Degree, knots, controlPts);
         }
+
+        /// <summary>
+        /// Elevates the degree of a NURBS curve.
+        /// <em>Implementation of Algorithm A5.9 of The NURBS Book by Piegl and Tiller.</em>
+        /// </summary>
+        /// <param name="curve">The object curve to elevate.</param>
+        /// <param name="finalDegree">The expected final degree. If the supplied degree is less or equal the curve is returned unmodified.</param>
+        /// <returns>The curve after degree elevation.</returns>
+        public static ICurve ElevateDegree(ICurve curve, int finalDegree)
+        {
+            if (finalDegree <= curve.Degree)
+            {
+                return curve;
+            }
+
+            int n = curve.Knots.Count - curve.Degree - 2;
+            int p = curve.Degree;
+            KnotVector U = curve.Knots;
+            List<Point4> Pw = curve.ControlPoints;
+            int t = finalDegree - curve.Degree; // Degree elevate a curve t times.
+
+
+            // local arrays.
+            double[,] bezalfs = new double[p + t + 1, p + 1];
+            Point4[] bpts = new Point4[p + 1];
+            Point4[] ebpts = new Point4[p + t + 1];
+            Point4[] nextbpts = new Point4[p - 1];
+            double[] alphas = new double[p - 1];
+
+            int m = n + p + 1;
+            int ph = finalDegree;
+            int ph2 = (int) Math.Floor((double) (ph / 2));
+
+            // Output values;
+            List<Point4> Qw = new List<Point4>();
+            KnotVector Uh = new KnotVector();
+
+            // Compute Bezier degree elevation coefficients.
+            bezalfs[0, 0] = bezalfs[ph, p] = 1.0;
+            for (int i = 1; i <= ph2; i++)
+            {
+                double inv = 1.0 / LinearAlgebra.GetBinomial(ph, i);
+                int mpi = Math.Min(p, i);
+
+                for (int j = Math.Max(0, i - t); j <= mpi; j++)
+                {
+                    bezalfs[i, j] = inv * LinearAlgebra.GetBinomial(p, j) * LinearAlgebra.GetBinomial(t, i - j);
+                }
+            }
+
+            for (int i = ph2 + 1; i <= ph-1; i++)
+            {
+                int mpi = Math.Min(p, i);
+                for (int j = Math.Max(0, i - t); j <= mpi; j++)
+                {
+                    bezalfs[i, j] = bezalfs[ph - i, p - j];
+                }
+            }
+
+            int mh = ph;
+            int kind = ph + 1;
+            int r = -1;
+            int a = p;
+            int b = p + 1;
+            int cind = 1;
+            double ua = U[0];
+            Qw.Add(Pw[0]);
+
+            for (int i = 0; i <=ph; i++)
+            {
+                Uh.Add(ua);
+            }
+
+            // Initialize first Bezier segment.
+            for (int i = 0; i <= p; i++)
+            {
+                bpts[i] = Pw[i];
+            }
+
+            // Big loop thru knot vector.
+            while (b < m)
+            {
+                int i = b;
+                while (b < m && Math.Abs(U[b] - U[b+1]) < GeoSharkMath.Epsilon)
+                {
+                    b += 1;
+                }
+
+                int mul = b - i + 1;
+                mh = mh + mul + t;
+                double ub = U[b];
+                int oldr = r;
+                r = p - mul;
+
+                // Insert knot U[b] r times.
+                // Checks for integer arithmetic.
+                int lbz = (oldr > 0) ? (int) Math.Floor((double) ((2 + oldr) / 2)) : 1;
+                int rbz = (r > 0) ? (int)Math.Floor((double)(ph - (r + 1) / 2)) : ph;
+
+                if (r > 0)
+                {
+                    // Inserts knot to get Bezier segment.
+                    double numer = ub - ua;
+                    List<double> alfs = new List<double>();
+                    for (int k = p; k > mul; k--)
+                    {
+                        alfs.Add(numer / (U[a + k] - ua));
+                    }
+                    for (int j = 1; j <= r; j++)
+                    {
+                        int save = r - j;
+                        int s = mul + j;
+                        for (int k = p; k >= s; k--)
+                        {
+                            bpts[k] = bpts[k] * alfs[k - s] + bpts[k - 1] * (1.0 - alfs[k - s]);
+                        }
+                        nextbpts[save] = bpts[p];
+                    }
+                }
+
+                // End of insert knot.
+                // Degree elevate Bezier.
+                for (int j = lbz; j <= ph; j++)
+                {
+                    ebpts[j] = Point4.Zero;
+                    int mpi = Math.Min(p, j);
+                    for (int k = Math.Max(0, j - t); k <= mpi; k++)
+                    {
+                        ebpts[j] += bpts[k] * bezalfs[j, k];
+                    }
+                }
+
+                if (oldr > 1)
+                {
+                    // Must remove knot u=U[a] oldr times.
+                    int first = kind - 2;
+                    int last = kind;
+                    double den = ub - ua;
+                    double bet = (ub - Uh[kind - 1]) / den;
+                    for (int tr = 1; tr < oldr; tr++)
+                    {
+                        // Knot removal loop.
+                        int ii = first;
+                        int jj = last;
+                        int kj = jj - kind + 1;
+
+                        while (jj - ii > tr)
+                        {
+                            // Loop and compute the new control points for one removal step.
+                            if (ii < cind)
+                            {
+                                double alf = (ub - Uh[ii]) / (ua - Uh[ii]);
+                                Qw.Add(Qw[ii] * alf + Qw[ii - 1] * (1.0 - alf));
+                            }
+
+                            if (jj >= lbz)
+                            {
+                                if (jj - tr <= kind - ph + oldr)
+                                {
+                                    double gam = (ub - Uh[jj - tr]) / den;
+                                    ebpts[kj] = ebpts[kj] * gam + ebpts[kj + 1] * (1.0 - gam);
+                                }
+                                else
+                                {
+                                    ebpts[kj] = ebpts[kj] * bet + ebpts[kj + 1] * (1.0 - bet);
+                                }
+                            }
+
+                            ii += 1;
+                            jj -= 1;
+                            kj -= 1;
+                        }
+
+                        first -= 1;
+                        last += 1;
+                    }
+                }
+
+                // End of removing knot, n = U[a].
+                if (a != p)
+                {
+                    // Load the knot ua.
+                    for (int j = 0; j < ph - oldr; j++)
+                    {
+                        Uh[kind] = ua;
+                        kind += 1;
+                    }
+                }
+
+                for (int j = lbz; j <= rbz; j++)
+                {
+                    // Load control points into Qw.
+                    Qw[cind] = ebpts[j];
+                    cind += 1;
+                }
+
+                if (b < m)
+                {
+                    // Set up for the next pass thru loop.
+                    for (int j = 0; j < r; j++)
+                    {
+                        bpts[j] = nextbpts[j];
+                    }
+
+                    for (int j = r; j <= p; j++)
+                    {
+                        bpts[j] = Pw[b - p + j];
+                    }
+
+                    a = b;
+                    b += 1;
+                    ua = ub;
+                }
+                else
+                {
+                    // End knot.
+                    for (int j = 0; j <= ph; j++)
+                    {
+                        Uh[kind + j] = ub;
+                    }
+                }
+            }
+
+            return new NurbsCurve(finalDegree, Uh, Qw);
+        }
     }
 }
