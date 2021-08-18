4bf3c2d7ad89f8ccee0727b9ca97daaa39f70951
Cesare Caoduro <Cesare.Caoduro@aecom.com>
2021-08-18 15:41:31 +1000

    Initiated PolyCurve

---
+++ src/GShark.Test.XUnit/Geometry/PolyCurveTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using GShark.Core;
+using GShark.Geometry;
+using GShark.Geometry.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace GShark.Test.XUnit.Geometry
+{
+    public class PolyCurveTests
+    {
+        private PolyCurve _polycurve;
+        private NurbsCurve _nurbs;
+        private Line _line;
+        private Arc _arc;
+
+        private readonly ITestOutputHelper _testOutput;
+
+        public PolyCurveTests(ITestOutputHelper testOutput)
+        {
+            _testOutput = testOutput;
+
+            int degree = 3;
+            List<Point3> pts = new List<Point3>
+            {
+                new Point3(0, 5, 5),
+                new Point3(0, 0, 0),
+                new Point3(5, 0, 0),
+                new Point3(5, 0, 5),
+                new Point3(5, 5, 5),
+                new Point3(5, 5, 0)
+            };
+
+            _nurbs = new NurbsCurve(pts, degree);
+            _line = new Line(new Point3(5, 5, 0), new Point3(5, 5, -2.5));
+            _arc = Arc.ByStartEndDirection(new Point3(5, 5, -2.5), new Point3(10, 5, -7.5), new Vector3(0, 0, -1));            
+        }
+
+        [Fact]
+        public void It_Returns_A_PolyCurve()
+        {
+            // Arrange
+            int numberOfExpectedSegments = 1;
+
+            // Act
+            _polycurve = new PolyCurve(_nurbs);
+
+            // Arrange
+            _polycurve.Should().NotBeNull();
+            _polycurve.SegmentCount.Should().Be(numberOfExpectedSegments);
+        }
+    }
+}
---
+++ src/GShark/Geometry/PolyCurve.cs
@@ -0,0 +1,111 @@
+﻿using GShark.Core;
+using GShark.Geometry.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GShark.Geometry
+{
+    /// <summary>
+    /// Represents a curve that is the result of joining several (possibly different) types of curves.
+    /// </summary>
+    public class PolyCurve : ICurve
+    {
+        /// <summary>
+        /// Initializes a newempty polycurve.
+        /// </summary>
+        public PolyCurve() { }
+
+        /// <summary>
+        /// Initializes a new polycurve from a curve.
+        /// </summary>
+        public PolyCurve(ICurve curve)
+        {
+            this.Segments.Add(curve);
+        }
+
+        /// <summary>
+        /// Appends and matches the start of the line to the end of polycurve.
+        /// </summary>
+        /// <param name="curve"></param>
+        /// <returns></returns>
+        public PolyCurve Append(Line line)
+        {
+            this.Segments.Add(line);
+            return this;
+        }
+
+        /// <summary>
+        /// Appends and matches the start of the arc to the end of polycurve.
+        /// </summary>
+        /// <param name="curve"></param>
+        /// <returns></returns>
+        public PolyCurve Append(Arc arc)
+        {
+            this.Segments.Add(arc);
+            return this;
+        }
+
+        /// <summary>
+        /// Appends and matches the start of the nurbs to the end of polycurve.
+        /// </summary>
+        /// <param name="curve"></param>
+        /// <returns></returns>
+        public PolyCurve Append(NurbsCurve nurbs)
+        {
+            this.Segments.Add(nurbs);
+            return this;
+        }
+
+        /// <summary>
+        /// Explodes this PolyCurve into a list of Curve segments.
+        /// </summary>
+        /// <returns></returns>
+        public ICollection<ICurve> Explode()
+        {
+            return this.Segments.Select(crv => crv).ToList();
+        }
+
+        /// <summary>
+        /// The segments of the PolyCurve
+        /// </summary>
+        public ICollection<ICurve> Segments => Explode();
+
+        /// <summary>
+        /// Number of segments
+        /// </summary>
+        public int SegmentCount => this.Segments.Count;
+
+        public int Degree => throw new NotImplementedException();
+
+        public List<Point3> LocationPoints => throw new NotImplementedException();
+
+        public List<Point4> ControlPoints => throw new NotImplementedException();
+
+        public KnotVector Knots => throw new NotImplementedException();
+
+        public Interval Domain => throw new NotImplementedException();
+
+        public BoundingBox BoundingBox => throw new NotImplementedException();
+
+        public Point3 ClosestPoint(Point3 pt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Point3 PointAt(double t)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Constructs the string representation for the current PolyCurve.
+        /// </summary>
+        /// <returns>The point representation in the form X,Y,Z,W.</returns>
+        public override string ToString()
+        {
+            return $"PolyCurve ({this.SegmentCount})";
+        }
+    }
+}
